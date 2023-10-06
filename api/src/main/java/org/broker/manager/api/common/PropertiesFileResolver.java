9d6991bda5c0bb2ea65e93a1e9a8cc3e0402d932
Alexander Kombeiz <alexkombie@gmail.com>
2023-10-06 11:56:27 +0200

    added interface for PropertiesReader and persistence handler

---
+++ api/src/main/java/org/broker/manager/api/common/PropertiesFileResolver.java
@@ -0,0 +1,8 @@
+package org.broker.manager.api.common;
+
+import org.broker.manager.api.enums.PropertiesKey;
+
+public interface PropertiesFileResolver {
+    
+    String getKeyValue(PropertiesKey key);
+}

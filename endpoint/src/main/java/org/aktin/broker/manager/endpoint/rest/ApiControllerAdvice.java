65caaa370d1c7618648f387928b646348a490d0a
Alexander Kombeiz <alexkombie@gmail.com>
2023-10-20 14:27:20 +0200

    init ManagerNodeController

---
+++ endpoint/src/main/java/org/aktin/broker/manager/endpoint/rest/ApiControllerAdvice.java
@@ -0,0 +1,10 @@
+package org.aktin.broker.manager.endpoint.rest;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@ControllerAdvice
+@RequestMapping("/api")
+public class ApiControllerAdvice {
+
+}

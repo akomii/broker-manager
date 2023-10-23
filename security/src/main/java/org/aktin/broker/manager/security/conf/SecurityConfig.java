be1680a5b6160732cb11b5cef6c651d801b48af1
Alexander Kombeiz <alexkombie@gmail.com>
2023-10-23 14:43:13 +0200

    added oauth2-resource-server dependency and configuration

--- security/src/main/java/org/aktin/broker/manager/security/conf/SecurityConfig.java
+++ security/src/main/java/org/aktin/broker/manager/security/conf/SecurityConfig.java
@@ -17,7 +17,6 @@ public class SecurityConfig {
 
   private static final String ERROR_PATH = "/error";
   private static final String LOGIN_PATH = "/login";
-  private static final String API_PATH = "/api/**";
   private static final String JSESSIONID_COOKIE = "JSESSIONID";
 
   @Value("${spring.security.oauth2.client.provider.broker-manager.logout-url}")
@@ -28,11 +27,12 @@ public class SecurityConfig {
     http.csrf(AbstractHttpConfigurer::disable);
     http.cors(AbstractHttpConfigurer::disable);
     http.authorizeHttpRequests(authorize -> authorize
-        .requestMatchers(ERROR_PATH, LOGIN_PATH, API_PATH)
+        .requestMatchers(ERROR_PATH, LOGIN_PATH)
         .permitAll()
         .anyRequest()
         .authenticated());
     http.oauth2Login(Customizer.withDefaults());
+    http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
     http.logout(
         logout -> logout
             .logoutSuccessHandler(

492224b734cc138cc628842ecd6ee7017837c877
Alexander Kombeiz <alexkombie@gmail.com>
2023-10-23 13:56:26 +0200

    removed var "id" from NodeStatusInfo, removed checkup annotations

--- api/src/main/java/org/aktin/broker/manager/api/factories/NodeStatusInfoFactory.java
+++ api/src/main/java/org/aktin/broker/manager/api/factories/NodeStatusInfoFactory.java
@@ -1,23 +1,20 @@
 package org.aktin.broker.manager.api.factories;
 
-import jakarta.annotation.Nonnull;
-import jakarta.annotation.Nullable;
 import java.time.Instant;
 import org.aktin.broker.manager.api.models.NodeStatusInfo;
 
 public interface NodeStatusInfoFactory extends GenericFactory<NodeStatusInfo> {
 
   NodeStatusInfo create(
-      @Nonnull Integer id,
-      @Nonnull Integer executionId,
-      @Nullable String statusMessage,
-      @Nonnull Integer node,
-      @Nullable Instant deleted,
-      @Nullable Instant retrieved,
-      @Nullable Instant queued,
-      @Nullable Instant processing,
-      @Nullable Instant completed,
-      @Nullable Instant rejected,
-      @Nullable Instant failed,
-      @Nullable Instant expired);
+      Integer node,
+      Integer executionId,
+      String statusMessage,
+      Instant deleted,
+      Instant retrieved,
+      Instant queued,
+      Instant processing,
+      Instant completed,
+      Instant rejected,
+      Instant failed,
+      Instant expired);
 }
--- persistence/src/main/java/org/aktin/broker/manager/persistence/factories/NodeStatusInfoFactoryImpl.java
+++ persistence/src/main/java/org/aktin/broker/manager/persistence/factories/NodeStatusInfoFactoryImpl.java
@@ -1,7 +1,5 @@
 package org.aktin.broker.manager.persistence.factories;
 
-import jakarta.annotation.Nonnull;
-import jakarta.annotation.Nullable;
 import java.time.Instant;
 import org.aktin.broker.manager.api.factories.NodeStatusInfoFactory;
 import org.aktin.broker.manager.api.models.NodeStatusInfo;
@@ -17,23 +15,21 @@ public class NodeStatusInfoFactoryImpl implements NodeStatusInfoFactory {
   }
 
   public NodeStatusInfo create(
-      @Nonnull Integer id,
-      @Nonnull Integer executionId,
-      @Nullable String statusMessage,
-      @Nonnull Integer node,
-      @Nullable Instant deleted,
-      @Nullable Instant retrieved,
-      @Nullable Instant queued,
-      @Nullable Instant processing,
-      @Nullable Instant completed,
-      @Nullable Instant rejected,
-      @Nullable Instant failed,
-      @Nullable Instant expired) {
+      Integer node,
+      Integer executionId,
+      String statusMessage,
+      Instant deleted,
+      Instant retrieved,
+      Instant queued,
+      Instant processing,
+      Instant completed,
+      Instant rejected,
+      Instant failed,
+      Instant expired) {
     NodeStatusInfoDao dao = new NodeStatusInfoDao();
-    dao.setId(id);
+    dao.setNode(node);
     dao.setExecutionId(executionId);
     dao.setStatusMessage(statusMessage);
-    dao.setNodeId(node);
     dao.setDeleted(deleted);
     dao.setRetrieved(retrieved);
     dao.setQueued(queued);

97d1135ac3fa3d2d953d4b90bd8092b6f058ec48
Alexander Kombeiz <alexkombie@gmail.com>
2023-10-23 12:48:41 +0200

    added factory method with initial values

--- api/src/main/java/org/aktin/broker/manager/api/factories/NodeStatusInfoFactory.java
+++ api/src/main/java/org/aktin/broker/manager/api/factories/NodeStatusInfoFactory.java
@@ -1,7 +1,23 @@
 package org.aktin.broker.manager.api.factories;
 
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import java.time.Instant;
 import org.aktin.broker.manager.api.models.NodeStatusInfo;
 
 public interface NodeStatusInfoFactory extends GenericFactory<NodeStatusInfo> {
 
+  NodeStatusInfo create(
+      @Nonnull Integer id,
+      @Nonnull Integer executionId,
+      @Nullable String statusMessage,
+      @Nonnull Integer node,
+      @Nullable Instant deleted,
+      @Nullable Instant retrieved,
+      @Nullable Instant queued,
+      @Nullable Instant processing,
+      @Nullable Instant completed,
+      @Nullable Instant rejected,
+      @Nullable Instant failed,
+      @Nullable Instant expired);
 }

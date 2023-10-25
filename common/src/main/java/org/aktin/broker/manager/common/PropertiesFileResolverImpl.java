b42e052e6ca1706cd05a07f3460da172d045cc49
Alexander Kombeiz <alexkombie@gmail.com>
2023-10-25 11:18:49 +0200

    refactored implementations in common a bit

--- common/src/main/java/org/aktin/broker/manager/common/PropertiesFileResolverImpl.java
+++ common/src/main/java/org/aktin/broker/manager/common/PropertiesFileResolverImpl.java
@@ -14,45 +14,63 @@ import org.aktin.broker.manager.api.common.PropertiesFileResolver;
 import org.aktin.broker.manager.api.enums.PropertiesKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PropertiesFileResolverImpl implements PropertiesFileResolver {
 
-  private final Logger logger = LoggerFactory.getLogger(PropertiesFileResolverImpl.class);
+  private static final Logger log = LoggerFactory.getLogger(PropertiesFileResolverImpl.class);
 
   private final Properties properties = new Properties();
 
-  private static final String DEFAULT_LINUX_PATH = "/etc/broker-manager/config.properties";
+  @Value("${broker-manager.config.default-linux-path}")
+  private String defaultLinuxPath;
 
   @PostConstruct
   public void init() {
-    loadProperties();
-    checkPropertiesFileForIntegrity();
+    try {
+      loadProperties();
+      checkPropertiesFileForIntegrity();
+    } catch (IOException e) {
+      throw new IllegalStateException("Failed to load properties", e);
+    }
   }
 
-  private void loadProperties() {
+  private void loadProperties() throws IOException {
     Path path = getPropertiesPath();
-    try (Reader in = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
-      properties.load(in);
-    } catch (IOException e) {
-      logger.error("Error during reading of properties file", e);
+    if (Files.exists(path)) {
+      try (Reader in = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+        properties.load(in);
+      }
+    } else {
+      throw new IllegalStateException(String.format("Properties file not found at %s", path));
     }
   }
 
   private Path getPropertiesPath() {
     String propertiesPath = System.getProperty("brokermanager.config.path");
-    return Paths.get(Objects.requireNonNullElse(propertiesPath, DEFAULT_LINUX_PATH));
+    if (propertiesPath == null) {
+      log.info(
+          "System property 'brokerbrokermanager.config.path' is null. Using default Linux path: {}",
+          defaultLinuxPath);
+    }
+    return Paths.get(Objects.requireNonNullElse(propertiesPath, defaultLinuxPath));
   }
 
-  private void checkPropertiesFileForIntegrity() throws IllegalStateException {
-    if (!Arrays.stream(PropertiesKey.values())
-        .allMatch(key -> properties.containsKey(key.toString()))) {
+  private void checkPropertiesFileForIntegrity() {
+    boolean allKeysPresent = Arrays.stream(PropertiesKey.values())
+        .allMatch(key -> properties.containsKey(key.toString()));
+    if (!allKeysPresent) {
       throw new IllegalStateException("Properties file is missing some keys");
     }
   }
 
   public String getKeyValue(PropertiesKey key) {
-    return properties.getProperty(key.toString());
+    String value = properties.getProperty(key.toString());
+    if (value == null) {
+      log.warn("Key {} not found in properties", key);
+    }
+    return value;
   }
 }

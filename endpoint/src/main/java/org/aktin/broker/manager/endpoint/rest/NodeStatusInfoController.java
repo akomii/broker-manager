0ed28f043a01568db412c95558566575e0569b1f
Alexander Kombeiz <alexkombie@gmail.com>
2023-10-25 10:12:51 +0200

    secured NodeStatusInfoController with user role

--- endpoint/src/main/java/org/aktin/broker/manager/endpoint/rest/NodeStatusInfoController.java
+++ endpoint/src/main/java/org/aktin/broker/manager/endpoint/rest/NodeStatusInfoController.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.aktin.broker.manager.api.models.NodeStatusInfo;
+import org.aktin.broker.manager.security.enums.UserRole.Code;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,26 +17,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//TODO test with keycloak
 //TODO NodeStatusInfoHandler
-
 @RestController
 @RequestMapping("/node-status-info")
 public class NodeStatusInfoController {
 
   private final List<NodeStatusInfo> infos = new ArrayList<>();
 
+  @Secured(Code.IT)
   @PostMapping
   public ResponseEntity<Void> create(@RequestBody NodeStatusInfo nodeStatusInfo) {
     infos.add(nodeStatusInfo);
     return new ResponseEntity<>(HttpStatus.CREATED);
   }
 
+  @Secured(Code.IT)
   @GetMapping
   public ResponseEntity<List<NodeStatusInfo>> getAll() {
     return new ResponseEntity<>(infos, HttpStatus.OK);
   }
 
+  @Secured(Code.IT)
   @GetMapping("/{id}")
   public ResponseEntity<NodeStatusInfo> get(@PathVariable Integer id) {
     Optional<NodeStatusInfo> statusInfo = findStatusInfoInList(id);
@@ -42,6 +45,7 @@ public class NodeStatusInfoController {
         .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
   }
 
+  @Secured(Code.IT)
   @PutMapping("/{id}")
   public ResponseEntity<Void> update(@PathVariable Integer id,
       @RequestBody NodeStatusInfo updatedNode) {
@@ -55,6 +59,7 @@ public class NodeStatusInfoController {
     }
   }
 
+  @Secured(Code.IT)
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> delete(@PathVariable Integer id) {
     Optional<NodeStatusInfo> infoToDelete = findStatusInfoInList(id);

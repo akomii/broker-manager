bb761c0d84f757b42eeb1436a9d4248c7d752725
Alexander Kombeiz <alexkombie@gmail.com>
2023-10-25 11:18:49 +0200

    refactored implementations in common a bit

--- common/src/main/java/org/aktin/broker/manager/common/BrokerAdminInitializerImpl.java
+++ common/src/main/java/org/aktin/broker/manager/common/BrokerAdminInitializerImpl.java
@@ -8,11 +8,15 @@ import org.aktin.broker.client2.auth.ApiKeyAuthentication;
 import org.aktin.broker.manager.api.common.BrokerAdminInitializer;
 import org.aktin.broker.manager.api.common.PropertiesFileResolver;
 import org.aktin.broker.manager.api.enums.PropertiesKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BrokerAdminInitializerImpl implements BrokerAdminInitializer {
+
+  private static final Logger log = LoggerFactory.getLogger(BrokerAdminInitializerImpl.class);
 
   @Autowired
   private PropertiesFileResolver propertiesFileResolver;
@@ -21,12 +25,17 @@ public class BrokerAdminInitializerImpl implements BrokerAdminInitializer {
 
   @PostConstruct
   public void init() {
-    URI brokerUri = URI.create(propertiesFileResolver.getKeyValue(PropertiesKey.URL));
-    String apiKey = propertiesFileResolver.getKeyValue(PropertiesKey.APIKEY);
-    this.brokerAdmin = createBrokerAdmin(brokerUri, apiKey);
+    this.brokerAdmin = initBrokerAdmin();
   }
 
-  private BrokerAdmin createBrokerAdmin(URI brokerUri, String apiKey) {
+  private BrokerAdmin initBrokerAdmin() {
+    String uriString = propertiesFileResolver.getKeyValue(PropertiesKey.URL);
+    String apiKey = propertiesFileResolver.getKeyValue(PropertiesKey.APIKEY);
+    if (uriString == null || apiKey == null) {
+      throw new IllegalStateException("Broker URI or API key is null. Initialization failed");
+    }
+    URI brokerUri = URI.create(uriString);
+    log.info("Initializing BrokerAdmin with URI: {}", brokerUri);
     BrokerAdmin2 admin = new BrokerAdmin2(brokerUri);
     admin.setAuthFilter(new ApiKeyAuthentication(apiKey));
     return admin;

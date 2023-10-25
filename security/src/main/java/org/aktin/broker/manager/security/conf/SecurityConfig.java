408a6cdfc3f7f860a603eea8bc76cd85f0085772
Alexander Kombeiz <alexkombie@gmail.com>
2023-10-25 09:47:37 +0200

    changed security config to resource-server - removed "resources" sub-folder - replaced Oauth2UserInfoMapper with JwtUserRoleConverter (some functionality but for jwt instead of oidc) - updated SecurityConfig for JwtUserRoleConverter - added test for JwtUserRoleConverter

--- security/src/main/java/org/aktin/broker/manager/security/conf/SecurityConfig.java
+++ security/src/main/java/org/aktin/broker/manager/security/conf/SecurityConfig.java
@@ -1,8 +1,8 @@
-package org.aktin.broker.manager.security.resources.conf;
+package org.aktin.broker.manager.security.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,12 +12,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
+  @Autowired
+  private JwtUserRoleConverter jwtUserRoleConverter;
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf(AbstractHttpConfigurer::disable);
     http.cors(AbstractHttpConfigurer::disable);
-    http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
-    http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+    http.authorizeHttpRequests(authorize ->
+        authorize.anyRequest().authenticated());
+    http.oauth2ResourceServer(oauth2 ->
+        oauth2.jwt(jwtConfigurer ->
+            jwtConfigurer.jwtAuthenticationConverter(jwtUserRoleConverter)));
     return http.build();
   }
 }

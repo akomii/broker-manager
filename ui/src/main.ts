baa44be15d73c734bfde713812308cdc40c766a5
Alexander Kombeiz <alexkombie@gmail.com>
2023-11-08 13:27:39 +0100

    updated vue versions, added lara-light-teal theme

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from "vue";
 import PrimeVue from "primevue/config";
 import App from "./App.vue";
-import "./theme.css";
-import 'primeicons/primeicons.css'
-
+import "primevue/resources/themes/lara-light-teal/theme.css";
+import "primeicons/primeicons.css";
 
 createApp(App).use(PrimeVue).mount("#app");

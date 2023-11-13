c4bad1a7c6f0566ed920f1e8e8b5157eeae992c0
Alexander Kombeiz <alexkombie@gmail.com>
2023-11-13 14:45:36 +0100

    Added simple component for RequestState

---
+++ ui/src/components/RequestState.vue
@@ -0,0 +1,35 @@
+<template>
+    <Tag :value="state" :severity="tagColor" rounded/>
+</template>
+  
+<script lang="ts">
+import Tag from 'primevue/tag';
+
+export default {
+    components: {
+        Tag,
+    },
+    props: {
+        state: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+        tagColor() {
+            switch (this.state) {
+                case 'DRAFT':
+                    return 'warning';
+                case 'ONLINE':
+                    return 'success';
+                case 'CLOSED':
+                    return 'danger';
+                case 'ARCHIVED':
+                    return 'info';
+                default:
+                    return 'secondary';
+            }
+        }
+    }
+};
+</script>

98972662a81805b25de7ee61df88f26c9aeb164e
Alexander Kombeiz <alexkombie@gmail.com>
2023-11-15 16:33:49 +0100

    added primeflex dependency

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import PrimeVue from "primevue/config";
 
+import PrimeVue from "primevue/config";
 import "primevue/resources/themes/md-light-deeppurple/theme.css";
 import "primeicons/primeicons.css";
+import 'primeflex/primeflex.css';
 
 createApp(App)
   .use(router)

2cfa62f691df28c34bba6223316fce40289a8267
Alexander Kombeiz <alexkombie@gmail.com>
2023-11-21 12:03:07 +0100

    added view component for principal

---
+++ ui/src/components/principal/Principal.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <Fieldset legend="Antragssteller" :toggleable="true" class="flex w-25rem m-2">
+            <PrincipalField label="Name" icon="pi pi-user" :value="principal.name" />
+            <PrincipalField label="Organization" icon="pi pi-building" :value="principal.organization" />
+            <PrincipalField label="Email" icon="pi pi-envelope" :value="principal.email" />
+            <PrincipalField label="Phone" icon="pi pi-phone" :value="principal.phone" />
+        </Fieldset>
+    </div>
+</template>
+
+<script lang="ts">
+import Fieldset from 'primevue/fieldset';
+import PrincipalField from '@/components/principal/PrincipalField.vue'
+
+export default {
+    components: {
+        Fieldset,
+        PrincipalField,
+    },
+    props: {
+        principal: Object,
+    },
+};
+</script>
---
+++ ui/src/components/principal/PrincipalField.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="field grid m-0">
+        <label :for="id" class="col-fixed w-3rem">
+            <i v-if="icon" :class="iconClass" class="text-2xl"></i>
+            <strong v-if="label && !icon">{{ label }}</strong>
+        </label>
+        <div class="col" :id="id">
+            {{ value }}
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        label: String,
+        icon: String,
+        value: String,
+    },
+    computed: {
+        id() {
+            return this.label.toLowerCase();
+        },
+        iconClass() {
+            return `${this.icon}`;
+        },
+    },
+};
+</script>

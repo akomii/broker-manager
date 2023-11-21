d4466f22754263cf7a9cb529f5cb71ad3d74f7bc
Alexander Kombeiz <alexkombie@gmail.com>
2023-11-21 17:12:15 +0100

    WIP editable principal fields

--- ui/src/components/principal/Principal.vue
+++ ui/src/components/principal/Principal.vue
@@ -1,25 +1,29 @@
 <template>
     <div>
         <Fieldset legend="Antragssteller" :toggleable="true" class="flex w-25rem m-2">
-            <PrincipalField label="Name" icon="pi pi-user" :value="principal.name" />
-            <PrincipalField label="Organization" icon="pi pi-building" :value="principal.organization" />
-            <PrincipalField label="Email" icon="pi pi-envelope" :value="principal.email" />
-            <PrincipalField label="Phone" icon="pi pi-phone" :value="principal.phone" />
+            <PrincipalField label="Name" icon="pi pi-user" :value="principal.name" :editable="editable" />
+            <PrincipalField label=" Organization" icon="pi pi-building" :value="principal.organization"
+                :editable="editable" />
+            <PrincipalField label="E-Mail" icon="pi pi-envelope" :value="principal.email" :editable="editable" />
+            <PrincipalField label="Telefon" icon="pi pi-phone" :value="principal.phone" :editable="editable" />
         </Fieldset>
     </div>
+
+    <!--TODO principal to real model -->
+    <!--TODO input validation -->
 </template>
 
 <script lang="ts">
 import Fieldset from 'primevue/fieldset';
 import PrincipalField from '@/components/principal/PrincipalField.vue'
-
 export default {
     components: {
         Fieldset,
         PrincipalField,
     },
     props: {
-        principal: Object,
-    },
+        principal: { type: Object, required: true, },
+        editable: { type: Boolean, default: false, },
+    }
 };
 </script>
--- ui/src/components/principal/PrincipalField.vue
+++ ui/src/components/principal/PrincipalField.vue
@@ -1,29 +1,52 @@
 <template>
-    <div class="field grid m-0">
-        <label :for="id" class="col-fixed w-3rem">
-            <i v-if="icon" :class="iconClass" class="text-2xl"></i>
-            <strong v-if="label && !icon">{{ label }}</strong>
-        </label>
-        <div class="col" :id="id">
-            {{ value }}
-        </div>
+    <div>
+        <template v-if="editable">
+            <InputGroup class="mb-2 h-3rem">
+                <InputGroupAddon>
+                    <i :class="iconClass" class="text-xl"></i>
+                </InputGroupAddon>
+                <span class="p-float-label">
+                    <InputText :id="fieldId" size="small" class="flex w-18rem" :modelValue="value"/>
+                    <label :for="fieldId">{{ label }}</label>
+                </span>
+            </InputGroup>
+        </template>
+
+        <template v-else>
+            <div class="field grid m-0">
+                <label :for="fieldId" class="col-fixed w-4rem">
+                    <i :class="iconClass" class="text-xl border-round surface-200 p-2 text-color-secondary"></i>
+                </label>
+                <div class="col pt-2" :id="fieldId">
+                    {{ value }}
+                </div>
+            </div>
+        </template>
     </div>
 </template>
 
 <script lang="ts">
+import InputGroup from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon';
+import InputText from 'primevue/inputtext';
+
 export default {
+    components: {
+        InputGroup,
+        InputGroupAddon,
+        InputText,
+    },
     props: {
-        label: String,
-        icon: String,
-        value: String,
+        label: { type: String, required: true, },
+        icon: { type: String, required: true, },
+        value: { type: String, required: true, },
+        editable: { type: Boolean, default: false, },
     },
     computed: {
-        id() {
-            return this.label.toLowerCase();
-        },
-        iconClass() {
-            return `${this.icon}`;
-        },
+        fieldId() { return this.label.toLowerCase(); },
+        iconClass() { return this.icon; }
     },
+
 };
 </script>
+  

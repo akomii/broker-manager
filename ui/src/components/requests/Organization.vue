e88ec9b0990ff6d8ed7911706fcff654244747ff
Alexander Kombeiz <alexkombie@gmail.com>
2023-11-27 14:18:21 +0100

    removed localModelValue, added sort after push()

--- ui/src/components/requests/Organization.vue
+++ ui/src/components/requests/Organization.vue
@@ -4,7 +4,7 @@
             <template v-if="editable">
                 <h3 class="underline m-1 text-center ">Ausgewählte Auswertestellen</h3>
                 <ScrollPanel class="w-24rem h-10rem">
-                    <template v-for="organization in selectedOrganizations">
+                    <template v-for="organization in modelValue">
                         <div class="hover:bg-red-100 flex justify-content-start flex-wrap"
                             @click="removeOrganization(organization)">
                             <i class="pi pi-minus-circle p-3 mt-1"></i>
@@ -26,7 +26,7 @@
             </template>
             <template v-else>
                 <ScrollPanel class="w-24rem max-h-11rem">
-                    <template v-for="organization in selectedOrganizations">
+                    <template v-for="organization in modelValue">
                         <div class="hover:surface-100">
                             <p class="text-left text-xl p-3 m-auto">{{ organization }}</p>
                         </div>
@@ -57,7 +57,6 @@ export default {
     data() {
         return {
             allOrganizations: [] as string[],
-            selectedOrganizations: this.modelValue as string[],
         };
     },
     mounted() {
@@ -68,13 +67,13 @@ export default {
     methods: {
         addOrganization(organization: string) {
             this.allOrganizations = this.allOrganizations.filter(org => org !== organization);
-            this.selectedOrganizations.push(organization);
-            this.$emit('update:modelValue', [...this.selectedOrganizations]);
+            const updatedModelValue = [...this.modelValue, organization].sort();
+            this.$emit('update:modelValue', updatedModelValue);
         },
         removeOrganization(organization: string) {
-            this.selectedOrganizations = this.selectedOrganizations.filter(org => org !== organization);
-            this.allOrganizations.push(organization);
-            this.$emit('update:modelValue', [...this.selectedOrganizations]);
+            const updatedModelValue = this.modelValue.filter(org => org !== organization).sort();
+            this.allOrganizations = [...this.allOrganizations, organization].sort();
+            this.$emit('update:modelValue', updatedModelValue);
         },
     }
 };

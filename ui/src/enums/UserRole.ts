d78955ef071d417213175a0bf7d8683d0ad9a7cf
Alexander Kombeiz <alexkombie@gmail.com>
2023-11-28 15:55:30 +0100

    merged and moved Enums to Utils

--- ui/src/enums/UserRole.ts
+++
@@ -1,7 +0,0 @@
-enum UserRole {
-    IT = 'ROLE_IT',
-    EC = 'ROLE_EC',
-  }
-  
-  export default UserRole;
-  
---
+++ ui/src/utils/Enums.ts
@@ -0,0 +1,18 @@
+export enum UserRole {
+  IT = "ROLE_IT",
+  EC = "ROLE_EC",
+}
+
+export enum RequestState {
+  DRAFT = "DRAFT",
+  ONLINE = "ONLINE",
+  CLOSED = "CLOSED",
+  ARCHIVED = "ARCHIVED",
+}
+
+export enum ExecutionState {
+  PENDING = "PENDING",
+  PUBLISHED = "PUBLISHED",
+  CLOSED = "CLOSED",
+  ARCHIVED = "ARCHIVED",
+}

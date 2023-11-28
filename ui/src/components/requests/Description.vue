bb462dd66d5ca23ae44e04cacb568e0e43a1ee62
Alexander Kombeiz <alexkombie@gmail.com>
2023-11-28 09:41:14 +0100

    Added component for request description

---
+++ ui/src/components/requests/Description.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <Fieldset legend="Beschreibung" :toggleable="true" class="flex w-auto m-2">
+            <template v-if="editable">
+                <Textarea class="h-30rem" v-model="localModelValue" @input="updateModelValue" />
+            </template>
+            <template v-else>
+                <ScrollPanel class="max-h-30rem" :pt="{
+                    wrapper: {
+                        style: { 'border-right': '10px solid var(--surface-ground)' }
+                    },
+                    bary: 'hover:bg-primary-400 bg-primary-300 opacity-100'
+                }">
+                    <p>{{ modelValue }}</p>
+                </ScrollPanel>
+            </template>
+        </Fieldset>
+    </div>
+    <!-- TODO Textarea widht -->
+    <!-- TODO Text nur ASCII -->
+</template>
+
+<script lang="ts">
+import Fieldset from 'primevue/fieldset';
+import Textarea from 'primevue/textarea';
+import ScrollPanel from 'primevue/scrollpanel';
+
+export default {
+    components: {
+        Fieldset,
+        ScrollPanel,
+        Textarea
+    },
+    props: {
+        modelValue: { type: String as () => string, required: true },
+        editable: { type: Boolean, default: false },
+    },
+    data() {
+        return {
+            localModelValue: this.modelValue as string,
+        };
+    },
+    methods: {
+        updateModelValue() {
+            this.$emit('update:modelValue', this.localModelValue);
+        }
+    }
+};
+</script>
+
+<style scoped>
+p {
+    white-space: pre-wrap;
+    text-align: justify;
+}
+.p-inputtextarea {
+    min-width: 370%;
+    max-width: 785%;
+}
+</style>

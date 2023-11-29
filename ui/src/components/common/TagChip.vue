f45578b252adff5df4eae5cf59b75890eb65af8f
Alexander Kombeiz <alexkombie@gmail.com>
2023-11-29 12:45:34 +0100

    added TagChip component

---
+++ ui/src/components/common/TagChip.vue
@@ -0,0 +1,18 @@
+<template>
+    <Chip class="py-1 px-2 m-1">
+        <span class="pi pi-tag"></span>
+        <span class="ml-1 text-sm">
+            <slot></slot>
+        </span>
+    </Chip>
+</template>
+
+<script lang="ts">
+import Chip from 'primevue/chip';
+
+export default {
+    components: {
+        Chip,
+    },
+}
+</script>

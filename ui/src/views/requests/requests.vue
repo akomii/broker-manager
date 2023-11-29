3f3b705cc114c913b5a9d2d3c3398e2dfa3dce18
Alexander Kombeiz <alexkombie@gmail.com>
2023-11-29 12:44:10 +0100

    WIP requestsTable

--- ui/src/views/requests/requests.vue
+++ ui/src/views/requests/requests.vue
@@ -1,77 +1,68 @@
 <template>
-    <div class="card">
-        <DataTable v-model:filters="filters" :value="requests" dataKey="id" stripedRows tableStyle="min-width: 50rem"
-            paginator :rows="10" :rowsPerPageOptions="[10, 20, 50]"
-            paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
-            currentPageReportTemplate="{first} bis {last} von {totalRecords}" removableSort sortField="id" :sortOrder="-1"
-            :globalFilterFields="['id', 'query.title', 'state', 'allowedOrganisations', 'seriesClosingDate']">
+    <DataTable v-model:filters="filters" :value="requests" ref="dt" dataKey="id" tableStyle="min-width: 50rem" paginator
+        :rows="10" :rowsPerPageOptions="[10, 20, 50]"
+        paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+        currentPageReportTemplate="{first} bis {last} von {totalRecords}" removableSort sortField="id" :sortOrder="-1"
+        :globalFilterFields="['id', 'query.title', 'state', 'allowedOrganisations', 'seriesClosingDate']">
 
-            <!-- TODO spinning loader -->
-            <!-- TODO lazy load -->
-            <!-- TODO store selected Columns in Cookie-->
+        <!-- TODO spinning loader -->
+        <!-- TODO lazy load -->
+        <!-- TODO store selected Columns in Cookie-->
 
-            <!-- TODO Column Toggle -->
-            <!-- TODO FIX COLUMN ELEMENTS -->
-            <!-- TODO PATH BREADCRUMBS -->
+        <!-- TODO Column Toggle -->
+        <!-- TODO nur eine column variable??? -->
+        <!--statische column reihenfolge-->
+        <!-- TODO FIX COLUMN ELEMENTS -->
+        <!--TODO globalfilterFields-->
+        <!-- global search nur auf die tabellen die eingeblendet sind -->
 
-            <template #header>
-                <Toolbar>
-                    <template #start>
+        <!-- TODOOOOOOO ROUTING IS BROKEN-->
 
-                        <div class="column-toggle">
-                            <Button type="button" icon="pi pi-cog" @click="toggle" />
+        <!-- export always all columns-->
 
-                            <OverlayPanel ref="opanel" appendTo="body" showCloseIcon>
-                                <div class="card flex flex-wrap justify-content-center gap-3">
-                                    <div class="flex align-items-center">
-                                        <Checkbox v-model="allSelected" inputId="selectAll" />
-                                        <label for="selectAll" class="ml-2">Select All</label>
-                                    </div>
-                                    <div class="flex align-items-center" v-for="(column, index) in columns" :key="index">
-                                        <Checkbox :value="column" v-model="selectedColumnsComputed"
-                                            :inputId="'column' + index" />
-                                        <label :for="'column' + index" class="ml-2">{{ column.header }}</label>
-                                    </div>
-                                </div>
-                            </OverlayPanel>
+        <!-- Dropdown-->
+        <!-- Ansicht -> Archivierte Anfragen anzeigen -->
+        <template #header>
+            <div class="flex justify-content-between flex-wrap border-solid">
+                <div class="border-solid">
+                    <h1 class="text-700">Anfragenübersicht</h1>
+                    <Button type="button" icon="pi pi-cog" outlined @click="toggle" class="mb-1 mr-3" />
+                    <OverlayPanel ref="columnToggle" showCloseIcon>
+                        <h3 class="underline mt-auto mb-1">Ansicht</h3>
+                        <div class="flex align-items-center">
+                            <!--  <Checkbox v-model="allSelected" inputId="selectAll" />
+                            <label for="selectAll" class="ml-2">Archivierte Anfragen anzeigen</label> -->
+                            <Checkbox />
+                            <label class="ml-2">Archivierte Anfragen anzeigen</label>
                         </div>
 
-                        <span class="p-input-icon-left">
-                            <i class="pi pi-search" />
-                            <InputText v-model="filters['global'].value" placeholder="Suche..." />
-                        </span>
-
-                        <!--
-                        <span class="p-float-label" style="margin-left: 1%">
-                            <MultiSelect :modelValue="selectedColumns" :options="columns" optionLabel="header" scrollHeight="300px" hide="true"
-                                @update:modelValue="onToggle" display="chip" placeholder="Spalten auswählen" >
-                                <Button label="Neuen Draft erstellen" icon="pi pi-plus" severity="success" />
-                            </MultiSelect>
-                            <label>Tabellenspalten</label>
-                        </span>
-                        -->
-
-                    </template>
-
-                    <template #end>
-                        <Button label="Neuen Draft erstellen" icon="pi pi-plus" severity="success" />
-                    </template>
-                </Toolbar>
-            </template>
+                        <h3 class="underline mt-auto mb-1 pt-3">Tabellenspalten</h3>
+                        <div class="flex align-items-center" v-for="(column, index) in columns" :key="index">
+                            <Checkbox :value="column" v-model="selectedColumnsComputed" :inputId="'column' + index" />
+                            <label :for="'column' + index" class="ml-2">{{ column.header }}</label>
+                        </div>
+                        <Divider layout="horizontal" class="my-2" />
+                        <div class="flex align-items-center">
+                            <Checkbox v-model="allSelected" inputId="selectAll" />
+                            <label for="selectAll" class="ml-2">Alle Spalten auswählen</label>
+                        </div>
+                    </OverlayPanel>
+                    <span class="p-input-icon-left">
+                        <i class="pi pi-search" />
+                        <InputText v-model="filters['global'].value" placeholder="Suche..." />
+                    </span>
+                </div>
+                <Button label="Neuen Draft erstellen" icon="pi pi-plus" severity="success" />
+            </div>
+        </template>
 
 
+        <template #empty>Keine Anfragen gefunden</template>
+        <Column v-for="(col, index) of selectedColumns" :field="col.field" :header="col.header"
+            :key="col.field + '_' + index" sortable></Column>
 
-
-
-
-
-            <template #empty>Keine Anfragen gefunden</template>
-
-            <Column v-for="(col, index) of selectedColumns" :field="col.field" :header="col.header"
-                :key="col.field + '_' + index" sortable></Column>
-
-            <!-- Column Toggle -->
-            <!--
+        <!-- Column Toggle -->
+        <!--
             <Column field="state" header="Status" sortable>
                 <template #body="slotProps">
                     <RequestState :state="slotProps.data.state" />
@@ -85,21 +76,30 @@
                 </template>
             </Column>
         -->
-            <Column headerStyle="width: 5rem; text-align: center" bodyStyle="text-align: center; overflow: visible">
-                <template #body>
-                    <Button type="button" icon="pi pi-search-plus" rounded outlined />
-                </template>
-            </Column>
 
 
-            
-        </DataTable>
-    </div>
+
+        <Column headerStyle="width: 5rem; text-align: center" bodyStyle="text-align: center; overflow: visible">
+            <template #body>
+                <Button type="button" icon="pi pi-search-plus" rounded outlined />
+            </template>
+        </Column>
+
+    </DataTable>
+    <Button icon="pi pi-external-link" outlined label="Tabelle exportieren" @click="exportCSV($event)" />
+
+
+    <!-- TODO tag element with option to make removable -->
+
+    <Chip class="py-1 px-2">
+        <span class="pi pi-tag"></span>
+        <span class="ml-1 font-small">tagging test</span>
+    </Chip>
 </template>
 
 <script lang="ts">
 import { TestDataService } from '@/service/TestDataService';
-import RequestState from '@/components/RequestState.vue';
+import RequestState from '@/components/requests/RequestState.vue';
 
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
@@ -112,6 +112,11 @@ import Row from 'primevue/row';
 import MultiSelect from 'primevue/multiselect';
 import OverlayPanel from 'primevue/overlaypanel';
 import Checkbox from 'primevue/checkbox';
+import Tag from 'primevue/tag';
+import Divider from 'primevue/divider';
+import Chip from 'primevue/chip';
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
 
 export default {
     components: {
@@ -126,7 +131,12 @@ export default {
         Row,
         MultiSelect,
         OverlayPanel,
-        Checkbox
+        Checkbox,
+        Tag,
+        Divider,
+        Chip,
+        Accordion,
+        AccordionTab,
     },
     data() {
         return {
@@ -135,6 +145,7 @@ export default {
             loading: true,
             selectedColumns: null,
             columns: null,
+            selectAll: false,
         };
     },
     computed: {
@@ -148,12 +159,14 @@ export default {
         },
         allSelected: {
             get() {
-                const allChecked = this.columns.length === this.selectedColumns.length;
-                console.log('All Selected:', allChecked); // Debugging
-                return allChecked;
+                return this.columns && this.selectedColumns && this.columns.length === this.selectedColumns.length;
             },
             set(value) {
-                this.selectedColumns = value ? this.columns : [];
+                if (value) {
+                    this.selectedColumns = this.columns.map(column => column);
+                } else {
+                    this.selectedColumns = [];
+                }
             }
         }
     },
@@ -163,11 +176,14 @@ export default {
         };
         this.columns = [
             { field: 'id', header: 'ID' },
+            { field: 'query.principal.name', header: 'Auftraggeber' },
             { field: 'query.title', header: 'Titel' },
+            { field: 'type', header: 'Typ' },
+            { field: 'tags', header: 'Tags' },
             { field: 'state', header: 'Status' },
             { field: 'allowedOrganisations', header: 'Auswertestelle(n)' },
-            { field: 'seriesClosingDate', header: 'Aktuellestes Veröffentlichungsdatum' },
-            { field: '', header: 'Aktuellestes Schließsdatum' },
+            { field: '', header: 'Aktuellestes Veröffentlichungsdatum' },
+            { field: 'seriesClosingDate', header: 'Aktuellestes Schließsdatum' },
             { field: '', header: 'Aktuelleste Zustimmung' },
         ];
         this.selectedColumns = this.columns.filter(column => ['id', 'query.title', 'state', 'allowedOrganisations'].includes(column.field));
@@ -188,14 +204,11 @@ export default {
             return new Intl.DateTimeFormat('de-DE', options).format(date);
         },
         toggle(event) {
-            this.$refs.opanel.toggle(event);
+            this.$refs.columnToggle.toggle(event);
+        },
+        exportCSV() {
+            this.$refs.dt.exportCSV();
         }
     }
 };
 </script>
-
-<style scoped>
-.column-toggle {
-    margin-right: 5%;
-}
-</style>

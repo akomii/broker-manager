fe4f41729c45384912650c90a3b2d442b14350c6
Alexander Kombeiz <alexkombie@gmail.com>
2023-11-30 09:45:53 +0100

    fixed 2-way-binding in EditTargetNodes, added sorting

--- ui/src/components/requests/targetNodes/EditTargetNodes.vue
+++ ui/src/components/requests/targetNodes/EditTargetNodes.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-wrap m-2">
-        <PickList v-model="products" dataKey="id" :pt="{
+        <PickList v-model="pickListNodes" dataKey="id" :pt="{
             sourceList: { style: { height: '350px', width: '360px' } },
             targetList: { style: { height: '350px', width: '360px' } },
             sourcecontrols: { style: { display: 'none' } },
@@ -10,7 +10,7 @@
             moveToSourceButton: { root: { class: 'bg-green-500' } },
             moveAllToSourceButton: { root: { class: 'bg-green-500' } },
             item: ({ context }) => ({ class: [{ 'bg-gray-100': !context.active && context.focused }] }),
-        }">
+        }" @update:modelValue="(pickList) => $emit('update:modelValue', pickList[0])">
             <template #sourceheader>Ausgewählte Standorte</template>
             <template #targetheader>Verfügbare Standorte</template>
             <template #item="slotProps">
@@ -26,37 +26,22 @@
                 </div>
             </template>
         </PickList>
-
         <!-- TODO color for items in source/target -->
-        <!-- TODO sort by ID -->
         <!-- TODO search -->
     </div>
+    {{ modelValue }}
 </template>
 
 <script lang="ts">
-import Fieldset from 'primevue/fieldset';
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-import Row from 'primevue/row';
-import { FilterMatchMode } from 'primevue/api';
-import InputText from 'primevue/inputtext';
-import Button from 'primevue/button';
-
-import { ManagerNode } from '@/utils/Types';
-import { TestDataService } from '@/service/TestDataService';
-import TagChip from '@/components/common/TagChip.vue';
 import PickList from 'primevue/picklist';
+import TagChip from '@/components/common/TagChip.vue';
+import { TestDataService } from '@/service/TestDataService';
+import { ManagerNode } from '@/utils/Types';
 
 export default {
     components: {
-        Fieldset,
-        DataTable,
-        Column,
-        Row,
-        InputText,
+        PickList,
         TagChip,
-        Button,
-        PickList
     },
     props: {
         modelValue: { type: Array as () => ManagerNode[], required: true },
@@ -64,41 +49,33 @@ export default {
     data() {
         return {
             allManagerNodes: [] as ManagerNode[],
-            localModelValue: [] as ManagerNode[],
-            filters: {
-                global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-            },
-            products: [[], []],
+            pickListNodes: [[] as ManagerNode[], [] as ManagerNode[]],
         };
     },
-    mounted() {
-        TestDataService.getNodes().then((data: ManagerNode[]) => {
-            this.allManagerNodes = data;
-            this.updateFilteredNodes();
-        });
+    computed: {
+        availableNodes() {
+            const selectedIds = new Set(this.modelValue.map(node => node.id));
+            return this.allManagerNodes.filter(node => !selectedIds.has(node.id)).sort((a, b) => a.id - b.id);
+        },
     },
-
     watch: {
         modelValue: {
             immediate: true,
             handler() {
-                this.updateFilteredNodes();
+                this.updatePickListNodes();
             },
         },
     },
+    mounted() {
+        TestDataService.getNodes().then((data: ManagerNode[]) => {
+            this.allManagerNodes = data;
+            this.updatePickListNodes();
+        });
+    },
     methods: {
-        updateFilteredNodes() {
-            if (this.modelValue && this.modelValue.length > 0) {
-                const modelValueIds = new Set(this.modelValue.map(node => node.id));
-                this.allManagerNodes = this.allManagerNodes.filter(node =>
-                    !modelValueIds.has(node.id)
-                );
-            }
-            this.localModelValue = this.modelValue || [];
-            this.products = [this.localModelValue, this.allManagerNodes];
-        },
-        updateModelValue() {
-            this.$emit('update:modelValue', this.localModelValue);
+        updatePickListNodes() {
+            const sortedModelValue = this.modelValue.slice().sort((a, b) => a.id - b.id);
+            this.pickListNodes = [sortedModelValue, this.availableNodes];
         },
     }
 };

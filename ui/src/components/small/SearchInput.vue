4be0918a9067cf3feefe88a53567aaf5e94ea36f
Alexander Kombeiz <alexkombie@gmail.com>
2023-12-05 13:20:15 +0100

    updated SearchInput to not rely on props

--- ui/src/components/small/SearchInput.vue
+++ ui/src/components/small/SearchInput.vue
@@ -1,12 +1,10 @@
 <template>
-    <div class="flex justify-content-end w-full">
-        <span class="p-input-icon-right">
-            <InputText v-model="localSearchTerm" @input="onSearchTermChange" placeholder="Suche..." class="w-15rem mb-2" />
-            <i class="pi pi-search" />
-        </span>
-    </div>
+    <span class="p-input-icon-right">
+        <InputText @input="onInput" placeholder="Suche..." class="w-15rem mb-2" />
+        <i class="pi pi-search" />
+    </span>
 </template>
-  
+
 <script lang="ts">
 import InputText from 'primevue/inputtext';
 
@@ -14,24 +12,9 @@ export default {
     components: {
         InputText
     },
-    props: {
-        searchTerm: {
-            type: String,
-            required: true
-        },
-        filterMethod: {
-            type: Function,
-            required: true
-        },
-    },
-    data() {
-        return {
-            localSearchTerm: this.searchTerm
-        };
-    },
     methods: {
-        onSearchTermChange() {
-            this.filterMethod(this.localSearchTerm);
+        onInput(event) {
+            this.$emit('inputChange', event.target.value);
         }
     }
 };

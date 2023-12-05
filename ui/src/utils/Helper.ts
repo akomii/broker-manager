6d576f87d40ede100643c8897f17fad29719feea
Alexander Kombeiz <alexkombie@gmail.com>
2023-12-05 11:33:50 +0100

    Added Helper.ts for utility functions

---
+++ ui/src/utils/Helper.ts
@@ -0,0 +1,12 @@
+export function formatToGermanDate(dateString: string): string {
+  const date = new Date(dateString);
+  const options: Intl.DateTimeFormatOptions = {
+    year: "2-digit",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  };
+  return new Intl.DateTimeFormat("de-DE", options).format(date);
+}

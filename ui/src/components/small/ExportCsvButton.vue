d27fe055ab5164424a80393f8f9f21c57fa9f0d3
Alexander Kombeiz <alexkombie@gmail.com>
2023-12-05 11:30:57 +0100

    Added custom Search and ExportCSV component

---
+++ ui/src/components/small/ExportCsvButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <Button icon="pi pi-external-link" label="Export" class="mt-2" @click="exportCSV" />
+</template>
+  
+<script lang="ts">
+import Button from 'primevue/button';
+
+export default {
+    components: {
+        Button
+    },
+    emits: ['exportCSV'],
+    methods: {
+        exportCSV() {
+            this.$emit('exportCSV');
+        }
+    }
+};
+</script>
---
+++ ui/src/components/small/SearchInput.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="flex justify-content-end w-full">
+        <span class="p-input-icon-right">
+            <InputText v-model="localSearchTerm" @input="onSearchTermChange" placeholder="Suche..." class="w-15rem mb-2" />
+            <i class="pi pi-search" />
+        </span>
+    </div>
+</template>
+  
+<script lang="ts">
+import InputText from 'primevue/inputtext';
+
+export default {
+    components: {
+        InputText
+    },
+    props: {
+        searchTerm: {
+            type: String,
+            required: true,
+        },
+        filterMethod: {
+            type: Function,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            localSearchTerm: this.searchTerm,
+        };
+    },
+    methods: {
+        onSearchTermChange() {
+            this.filterMethod(this.localSearchTerm);
+        }
+    }
+};
+</script>

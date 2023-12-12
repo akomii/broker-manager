0787bf11fbad898faff86ad747e46887dc570086
Alexander Kombeiz <alexkombie@gmail.com>
2023-12-12 21:49:20 +0100

    merged all Headers into one component

---
+++ ui/src/components/requests/Header.vue
@@ -0,0 +1,139 @@
+<template>
+    <div class="mx-2">
+        <div class="flex flex-wrap justify-content-between">
+            <div class="flex flex-wrap justify-content-start align-items-center">
+                <Button class="mx-2" icon="pi pi-chevron-left text-xl" outlined />
+
+                <!-- Title Section -->
+                <div class="flex flex-wrap justify-content-start align-items-center">
+                    <p v-if="id !== undefined" class="text-2xl mx-2">[{{ id }}]</p>
+                    <template v-if="editable && state === 'DRAFT'">
+                        <span class="p-float-label">
+                            <InputText size="large" class="flex w-30rem text-2xl" v-model="localTitle" />
+                            <label>Titel</label>
+                        </span>
+                    </template>
+                    <p v-else class="text-2xl mx-1">{{ title }}</p>
+                </div>
+
+                <!-- Request State -->
+                <div class="mx-2">
+                    <RequestState class="text-lg" :state="state" />
+                </div>
+
+                <!-- Tags Section -->
+                <div class="flex flex-wrap max-w-30rem mx-2">
+                    <TagList :tags="editable ? localTags : tags" :removable="editable" @update:tags="localTags = $event" />
+                    <TagCreator v-if="editable" :tags="localTags" @update:tags="localTags = $event" />
+                </div>
+            </div>
+
+            <!-- Menu Button -->
+            <div class="flex flex-wrap justify-content-end align-items-center">
+                <div class="mx-2">
+                    <div v-if="editable">
+                        <MenuButton :icon="'pi pi-chevron-down'"
+                            :menu="state === 'DRAFT' ? editDraftMenu : editRequestMenu" />
+                    </div>
+                    <div v-else>
+                        <MenuButton :icon="'pi pi-chevron-down'"
+                            :menu="state === 'DRAFT' ? draftMenu : (hasUserRoleIT ? requestMenuIT : requestMenuDAC)" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <Divider class="mt-0" />
+    </div>
+</template>
+
+<script lang="ts">
+import Button from 'primevue/button';
+import Divider from 'primevue/divider';
+import InputText from 'primevue/inputtext';
+
+import TagList from '@/components/small/tags/TagList.vue';
+import TagCreator from '@/components/small/tags/TagCreator.vue';
+import MenuButton from '@/components/small/buttons/MenuButton.vue';
+import RequestState from '@/components/small/RequestState.vue';
+import { UserRole } from '@/utils/Enums.ts';
+
+export default {
+    components: {
+        Button,
+        Divider,
+        InputText,
+        TagList,
+        TagCreator,
+        MenuButton,
+        RequestState
+    },
+    props: {
+        id: {
+            type: Number,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        state: {
+            type: String,
+            required: true,
+        },
+        tags: {
+            type: Set<string>,
+            required: true
+        },
+        editable: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            draftMenu: [
+                { label: 'Draft als Anfrage veröffentlichen' },
+                { label: 'Draft duplizieren' },
+                { label: 'Draft löschen' },
+                { label: 'Draft bearbeiten' },
+            ],
+            editDraftMenu: [
+                { label: 'Draft speichern' },
+                { label: 'Abbrechen' },
+            ],
+            requestMenuIT: [
+                { label: 'Ergebnisübersicht' },
+                { label: 'Als Draft duplizieren' },
+                { label: 'Anfrage schließen' },
+                { label: 'Anfrage archivieren' },
+                { label: 'Anfrage bearbeiten' },
+            ],
+            requestMenuDAC: [
+                { label: 'Ergebnisübersicht' },
+                { label: 'Anfrage schließen' },
+                { label: 'Anfrage archivieren' },
+            ],
+            editRequestMenu: [
+                { label: 'Anfrage speichern' },
+                { label: 'Abbrechen' },
+            ],
+            localTitle: this.title,
+            localTags: this.tags,
+            userRole: UserRole.IT // TODO: grab userRole from Keycloak response
+        };
+    },
+    computed: {
+        hasUserRoleIT() {
+            return this.userRole === UserRole.IT;
+        }
+    },
+    watch: {
+        localTitle(newTitle) {
+            this.$emit('update:title', newTitle);
+        },
+        localTags(updatedTags) {
+            this.$emit('update:tags', updatedTags);
+        }
+    }
+};
+</script>
--- ui/src/views/clinics/clinics.vue
+++ ui/src/views/clinics/clinics.vue
@@ -1,19 +1,34 @@
 <template>
     <div v-if="request">
-        <div class="flex justify-content-center flex-wrap mt-5">
+        <div class="flex justify-content-center flex-wrap mt-5 mb-8">
             <Button :label="editable ? 'Änderungen speichern' : 'Antragssteller editieren'" @click="click" />
         </div>
-        <Divider/>
-        <TargetNodes v-model="request.targetNodes" :editable="editable"></TargetNodes>
-        <Divider/>
-        <SQL v-model="request.query.sql" :editable="editable"></SQL>
-        <Divider/>
-        <Description v-model="request.query.description" :editable="editable"></Description>
-        <Divider/>
-        <Organization v-model="request.authorizedOrgs" :editable="editable"></Organization>
-        <Divider/>
-        <Principal v-model="request.query.principal" :editable="editable" />
-        <Divider/>
+
+        <Header :id="request.id" :title="request.query.title" :state="'DRAFT'" :tags="request.tags" :editable="false" />
+
+        <Header :id="request.id" :title="request.query.title" :state="'DRAFT'" :tags="request.tags" :editable="true"
+            @update:tags="request.tags = $event" @update:title="request.query.title = $event" />
+
+        <Header :id="request.id" :title="request.query.title" :state="'ONLINE'" :tags="request.tags" :editable="false" />
+
+        <Header :id="request.id" :title="request.query.title" :state="'ONLINE'" :tags="request.tags" :editable="true"
+            @update:tags="request.tags = $event" @update:title="request.query.title = $event" />
+
+        <!--
+        <div class="grid">
+            <div class="col">
+                <DraftTargetNodes v-model="request.targetNodes" :editable="editable" />
+            </div>
+            <Divider layout="vertical" />
+            <div class="col">
+                <RequestTargetNodes v-model="request.targetNodes" :execution="request.executions[2]" :editable="editable" />
+            </div>
+        </div>
+            <Principal v-model="request.query.principal" :editable="editable" />
+            <Organization v-model="request.authorizedOrgs" :editable="editable"></Organization>
+            <SQL v-model="request.query.sql" :editable="editable"></SQL>
+            <Description v-model="request.query.description" :editable="editable"></Description>
+            -->
     </div>
     <div v-else class="flex justify-content-center flex-wrap py-4">
         <ProgressSpinner />
@@ -25,13 +40,15 @@ import Principal from '@/components/requests/principal/Principal.vue';
 import Organization from '@/components/requests/Organization.vue';
 import Description from '@/components/requests/Description.vue';
 import SQL from '@/components/requests/Sql.vue';
-import TargetNodes from '@/components/requests/targetNodes/DraftTargetNodes.vue';
+import DraftTargetNodes from '@/components/requests/targetNodes/DraftTargetNodes.vue';
+import RequestTargetNodes from '@/components/requests/targetNodes/RequestTargetNodes.vue';
 import Button from 'primevue/button';
 import ProgressSpinner from 'primevue/progressspinner';
 import Divider from 'primevue/divider';
 
 import { TestDataService } from '@/service/TestDataService';
 import { Request } from '@/utils/Types';
+import Header from '@/components/requests/Header.vue';
 
 export default {
     components: {
@@ -39,10 +56,12 @@ export default {
         Button,
         Organization,
         Description,
-        TargetNodes,
+        DraftTargetNodes,
+        RequestTargetNodes,
         ProgressSpinner,
         Divider,
-        SQL
+        SQL,
+        Header
     },
     data() {
         return {
@@ -62,7 +81,7 @@ export default {
     methods: {
         click() {
             this.editable = !this.editable;
-        },
+        }
     }
 };
 </script>

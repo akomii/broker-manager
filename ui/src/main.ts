c3bb93506966eeb2741aa85d0beef4df529a0558
Alexander Kombeiz <alexkombie@gmail.com>
2023-12-12 17:47:50 +0100

    added ToastService

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 
 import PrimeVue from "primevue/config";
+import ToastService from 'primevue/toastservice';
 import "primevue/resources/themes/mdc-light-deeppurple/theme.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
@@ -10,4 +11,5 @@ import "primeflex/primeflex.css";
 createApp(App)
   .use(router)
   .use(PrimeVue, { inputStyle: "filled" })
+  .use(ToastService)
   .mount("#app");

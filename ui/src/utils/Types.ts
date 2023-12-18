1eacbc8b27f9d32cad37e0cd50348f566ec5eea5
Alexander Kombeiz <alexkombie@gmail.com>
2023-12-18 10:46:10 +0100

    Refactor Types.ts interface and export modifications

--- ui/src/utils/Types.ts
+++ ui/src/utils/Types.ts
@@ -4,115 +4,115 @@ import { MomentDuration } from "@/utils/MomentWrapper";
 export type Request = SingleRequest | SeriesRequest;
 
 interface ManagerRequest {
-  id: number;
-  tags: Set<string>;
-  authorizedOrgs: Set<number>; // Organization IDs
-  targetNodes: Set<number>; // ManagerNode IDs
-  requestState: RequestState;
-  modificationHistory: ModificationHistoryItem[];
-  query: Query;
-  executions: RequestExecution[];
+    id: number;
+    tags: Set<string>;
+    authorizedOrgs: Set<number>; // Organization IDs
+    targetNodes: Set<number>; // ManagerNode IDs
+    requestState: RequestState;
+    modificationHistory: ModificationHistoryItem[];
+    query: Query;
+    executions: RequestExecution[];
 }
 
-interface SingleRequest extends ManagerRequest {}
+interface SingleRequest extends ManagerRequest { }
 
 interface SeriesRequest extends ManagerRequest {
-  anchoredSequenceIdRef: number;
-  isAutoPublishing: boolean;
-  seriesClosingDate: Date | null;
-  seriesArchiveDate: Date | null;
+    anchoredSequenceIdRef: number;
+    isAutoPublishing: boolean;
+    seriesClosingDate: Date | null;
+    seriesArchiveDate: Date | null;
 }
 
 export interface Organization {
-  id: number;
-  name: string;
+    id: number;
+    name: string;
 }
 
-interface ModificationHistoryItem {
-  date: Date;
-  user: string;
-  clob: string;
+export interface ModificationHistoryItem {
+    date: Date;
+    user: string;
+    clob: string;
 }
 
 interface Query {
-  title: string;
-  description: string;
-  sql: string;
-  principal: Principal;
-  singleExecution?: SingleExecution;
-  repeatedExecution?: RepeatedExecution;
+    title: string;
+    description: string;
+    sql: string;
+    principal: Principal;
+    singleExecution?: SingleExecution;
+    repeatedExecution?: RepeatedExecution;
 }
 
 export interface Principal {
-  [key: string]: string | null;
-  name: string;
-  organization: string;
-  email: string;
-  phone: string | null;
+    [key: string]: string | null;
+    name: string;
+    organization: string;
+    email: string;
+    phone: string | null;
 }
 
 interface SingleExecution {
-  duration: MomentDuration;
+    duration: MomentDuration;
 }
 
 // TODO check if interval/intervalHours is necessary
 interface RepeatedExecution {
-  id: number;
-  duration: MomentDuration;
-  interval: number;
-  intervalHours: number;
+    id: number;
+    duration: MomentDuration;
+    interval: number;
+    intervalHours: number;
 }
 
 export interface RequestExecution {
-  sequenceId: number;
-  externalId: number | null;
-  referenceDate: Date;
-  executionDate: Date;
-  scheduledPublishDate: Date;
-  publishedDate: Date | null;
-  scheduledClosingDate: Date;
-  closedDate: Date | null;
-  scheduledArchiveDate: Date;
-  archivedDate: Date | null;
-  creator: string;
-  createdDate: Date;
-  executionState: ExecutionState;
-  nodeStatusInfos: NodeStatusInfo[];
-  resultsDownloadLog: ResultsDownloadLog[];
+    sequenceId: number;
+    externalId: number | null;
+    referenceDate: Date;
+    executionDate: Date;
+    scheduledPublishDate: Date;
+    publishedDate: Date | null;
+    scheduledClosingDate: Date;
+    closedDate: Date | null;
+    scheduledArchiveDate: Date;
+    archivedDate: Date | null;
+    creator: string;
+    createdDate: Date;
+    executionState: ExecutionState;
+    nodeStatusInfos: NodeStatusInfo[];
+    resultsDownloadLog: ResultsDownloadLog[];
 }
 
 export interface NodeStatusInfo {
-  nodeId: number;
-  statusMessage: string | null;
-  deleted: Date | null;
-  retrieved: Date | null;
-  queued: Date | null;
-  processing: Date | null;
-  completed: Date | null;
-  rejected: Date | null;
-  failed: Date | null;
-  expired: Date | null;
+    nodeId: number;
+    statusMessage: string | null;
+    deleted: Date | null;
+    retrieved: Date | null;
+    queued: Date | null;
+    processing: Date | null;
+    completed: Date | null;
+    rejected: Date | null;
+    failed: Date | null;
+    expired: Date | null;
 }
 
-interface ResultsDownloadLog {
-  user: string;
-  userOrgs: Set<number>; // Organization IDs
-  date: Date;
-  hashValue: string;
-  hashAlgorithm: string;
+export interface ResultsDownloadLog {
+    user: string;
+    userOrgs: Set<number>; // Organization IDs
+    date: Date;
+    hashValue: string;
+    hashAlgorithm: string;
 }
 
 export interface ManagerNode {
-  id: number;
-  tags: Set<string>;
-  clientDN: ClientDN;
-  lastContact: Date;
-  apiKey: string;
-  notes: { [key: string]: string };
+    id: number;
+    tags: Set<string>;
+    clientDN: ClientDN;
+    lastContact: Date;
+    apiKey: string;
+    notes: Record<string, string>;
 }
 
 interface ClientDN {
-  CN: string;
-  O: string;
-  L: string;
+    CN: string;
+    O: string;
+    L: string;
 }

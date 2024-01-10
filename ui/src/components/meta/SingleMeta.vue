e7d8121def7cfe2ffa91640b65e31958877f433f
Alexander Kombeiz <alexkombie@gmail.com>
2024-01-10 14:27:04 +0100

    WIP SingleMeta, added reference date selection and static request information

--- ui/src/components/meta/SingleMeta.vue
+++ ui/src/components/meta/SingleMeta.vue
@@ -1,6 +1,23 @@
 <template>
     <div>
         <Fieldset legend="Meta" :toggleable="true" class="flex w-auto m-2">
+            <p>Typ</p>
+            <EnumState
+                class="text-sm"
+                :state="RequestType.SINGLE"
+                :stateColorMap="requestTypeColorMap"
+            />
+
+            <p>Externe ID</p>
+            <p>{{ execution.externalId }}</p>
+
+            <p>Ausführungsstatus</p>
+            <EnumState
+                class="text-sm"
+                :state="execution.executionState"
+                :stateColorMap="requestStateColorMap"
+            />
+
             <p>Veröffentlichungsdatum</p>
             <ScheduledDatePicker
                 :label="'Veröffentlichungsdatum'"
@@ -32,6 +49,21 @@
                 @update:scheduledDate="execution.scheduledArchiveDate = $event"
                 :editable="editable"
             />
+
+            <p>Referenzzeitraum</p>
+            <DatePicker
+                :label="'Referenzdatum Start'"
+                :date="execution.referenceDate"
+                @update:date="execution.referenceDate = $event"
+                :editable="editable"
+            />
+            <br />
+            <DatePicker
+                :label="'Referenzdatum Ende'"
+                :date="dummyDate"
+                @update:date="dummyDate = $event"
+                :editable="editable"
+            />
         </Fieldset>
     </div>
 </template>
@@ -39,19 +71,31 @@
 <script lang="ts">
 import Fieldset from "primevue/fieldset";
 
-import { RequestExecution } from "@/utils/Types";
+import { RequestExecution, SingleExecution } from "@/utils/Types";
 import ScheduledDatePicker from "@/components/datePickers/ScheduledDatePicker.vue";
 import DatePicker from "@/components/datePickers/DatePicker.vue";
+import MomentWrapper from "@/utils/MomentWrapper";
+import EnumState from "@/components/states/EnumState.vue";
+import {
+    requestTypeColorMap,
+    requestStateColorMap,
+} from "@/utils/Constants.ts";
+import { RequestType } from "@/utils/Enums.ts";
 
 export default {
     components: {
         Fieldset,
         ScheduledDatePicker,
         DatePicker,
+        EnumState,
     },
     props: {
         execution: {
             type: Object as () => RequestExecution,
+            required: true,
+        },
+        querySchedule: {
+            type: Object as () => SingleExecution,
             required: true,
         },
         editable: {
@@ -60,9 +104,40 @@ export default {
         },
     },
     data() {
-        return {};
+        return {
+            RequestType,
+            requestTypeColorMap,
+            requestStateColorMap,
+            dummyDate: new Date(),
+        };
     },
-    watch: {},
-    methods: {},
+    computed: {
+        duration() {
+            return MomentWrapper.computePeriod(
+                this.dummyDate,
+                this.execution.referenceDate
+            );
+        },
+    },
+    watch: {
+        "execution.scheduledPublishDate": function () {
+            this.$emit("update:execution", this.execution);
+        },
+        "execution.executionDate": function () {
+            this.$emit("update:execution", this.execution);
+        },
+        "execution.scheduledClosingDate": function () {
+            this.$emit("update:execution", this.execution);
+        },
+        "execution.scheduledArchiveDate": function () {
+            this.$emit("update:execution", this.execution);
+        },
+        "execution.referenceDate": function () {
+            this.$emit("update:execution", this.execution);
+        },
+        duration(newVal) {
+            this.$emit("update:querySchedule", newVal);
+        },
+    },
 };
 </script>

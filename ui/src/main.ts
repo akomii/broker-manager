574007c5167c11d4f104a3b75355235d7b996085
Alexander Kombeiz <alexkombie@gmail.com>
2024-01-11 13:14:39 +0100

    Add vue-i18n package and configure localization

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
+import { createI18n } from "vue-i18n";
 import App from "@/App.vue";
 import router from "@/router";
-import GermanLocale from "@/locales/de.json";
+import de from "@/locales/de.json";
 
 import PrimeVue from "primevue/config";
 import ToastService from "primevue/toastservice";
@@ -10,9 +11,15 @@ import "primevue/resources/themes/mdc-light-deeppurple/theme.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 
+const i18n = createI18n({
+    locale: "de",
+    messages: { de },
+});
+
 createApp(App)
     .use(router)
-    .use(PrimeVue, { inputStyle: "filled", locale: GermanLocale })
+    .use(i18n)
+    .use(PrimeVue, { inputStyle: "filled", locale: de })
     .use(ToastService)
     .directive("tooltip", Tooltip)
     .mount("#app");

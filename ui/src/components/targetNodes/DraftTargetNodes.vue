ad97d87433321bf723e63b6abc307f005044585b
Alexander Kombeiz <alexkombie@gmail.com>
2024-01-22 11:52:53 +0100

    WIP DraftTargetNodes

--- ui/src/components/targetNodes/DraftTargetNodes.vue
+++ ui/src/components/targetNodes/DraftTargetNodes.vue
@@ -1,60 +1,77 @@
 <template>
-    <template v-if="editable">
+    <Fieldset :legend="$t('targetNodes')" :toggleable="true">
+        <template v-if="editable">
+            <!--
         <EditTargetNodes
-            :modelValue="modelValue"
-            @update:modelValue="handleModelValueChange"
+            :modelValue="targetNodeIds"
+            @update:modelValue="onTargetNodeIdsChange"
             :allManagerNodes="allManagerNodes"
         />
-    </template>
-    <template v-else>
-        <div class="flex flex-wrap w-full justify-content-end align-items-end">
-            <SearchInput @inputChange="updateGlobalFilter" />
-        </div>
-        <DataTable
-            ref="dt"
-            v-model:filters="filters"
-            :value="selectedNodes"
-            tableStyle="min-width: 50rem"
-            sortField="id"
-            :sortOrder="1"
-            :globalFilterFields="['id', 'clientDN.L', 'tags', 'lastContact']"
-            scrollable
-            scrollHeight="500px"
-        >
-            <Column field="id" header="ID" sortable></Column>
-            <Column field="clientDN.L" header="Standort" sortable></Column>
-            <Column field="tags" header="Tags" sortable>
-                <template #body="slotProps">
-                    <div class="p-flex p-flex-wrap">
+        -->
+        </template>
+        <template v-else>
+            <div class="flex justify-content-end">
+                <SearchInput @update:input="updateGlobalFilter" />
+            </div>
+
+            <DataTable
+                ref="dt"
+                v-model:filters="filters"
+                :value="selectedNodes"
+                sortField="id"
+                :sortOrder="1"
+                :globalFilterFields="[
+                    'id',
+                    'clientDN.L',
+                    'tags',
+                    'lastContact',
+                    'state'
+                ]"
+                scrollable
+            >
+                <Column field="id" :header="$t('id')" sortable />
+                <Column field="clientDN.L" :header="$t('node')" sortable />
+                <Column field="tags" :header="$t('tags')" sortable>
+                    <template #body="slotProps">
                         <TagList :tags="slotProps.data.tags" />
-                    </div>
-                </template>
-            </Column>
-            <Column field="lastContact" header="Letzter Kontakt" sortable>
-                <template #body="slotProps">
-                    {{ formatDateToGermanLocale(slotProps.data.lastContact) }}
-                </template>
-            </Column>
-        </DataTable>
-        <ExportTableButton :datatableRef="$refs.dt" />
-    </template>
+                    </template>
+                </Column>
+                <Column
+                    field="lastContact"
+                    :header="$t('lastContact')"
+                    sortable
+                >
+                    <template #body="slotProps">
+                        {{
+                            formatDateToGermanLocale(slotProps.data.lastContact)
+                        }}
+                    </template>
+                </Column>
+            </DataTable>
+            <!-- TODO make globalFilterFields conditionally -->
+            <!-- TODO Search does not work for state -->
+            <!-- TODO Search does not work for date -->
+
+            <!-- Filter by Tag does not work -->
+            <!-- ToDo Somehow does not work???-->
+            <ExportTableButton class="mt-3" :datatableRef="$refs.dt" />
+        </template>
+    </Fieldset>
 </template>
 
 <script lang="ts">
 import Fieldset from "primevue/fieldset";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
-import Row from "primevue/row";
 import { FilterMatchMode } from "primevue/api";
-import InputText from "primevue/inputtext";
-import Button from "primevue/button";
 
-import { ManagerNode } from "@/utils/Types";
-import { TestDataService } from "@/services/TestDataService";
 import TagList from "@/components/tags/TagList.vue";
-import ExportTableButton from "@/components/buttons/ExportTableButton.vue";
-import EditTargetNodes from "@/components/targetNodes/EditTargetNodes.vue";
+import { TestDataService } from "@/services/TestDataService";
+import { RequestState } from "@/utils/Enums";
+import { ManagerNode, RequestExecution, NodeStatusInfo } from "@/utils/Types";
 import MomentWrapper from "@/utils/MomentWrapper";
+import ExportTableButton from "./ExportTableButton.vue";
+import EditTargetNodes from "./EditTargetNodes.vue";
 import SearchInput from "./SearchInput.vue";
 
 export default {
@@ -62,17 +79,22 @@ export default {
         Fieldset,
         DataTable,
         Column,
-        Row,
-        InputText,
         TagList,
-        Button,
         EditTargetNodes,
         ExportTableButton,
         SearchInput,
     },
     props: {
-        modelValue: {
-            type: Array as () => number[],
+        targetNodeIds: {
+            type: Set<number>,
+            required: true,
+        },
+        execution: {
+            type: Object as () => RequestExecution,
+            required: true,
+        },
+        requestState: {
+            type: String as () => keyof typeof RequestState,
             required: true,
         },
         editable: {
@@ -89,26 +111,48 @@ export default {
         };
     },
     computed: {
-        selectedNodes() {
+        selectedNodes(): ManagerNode[] {
             return this.allManagerNodes.filter((node) =>
-                this.modelValue.includes(node.id)
+                this.targetNodeIds.has(node.id)
             );
         },
+        requestExecutionOnNodes() {
+            return this.execution.nodeStatusInfos.filter(
+                (nodeInfo) => nodeInfo.completed !== null
+            ).length;
+        },
     },
-    mounted() {
-        TestDataService.getNodes().then((data: ManagerNode[]) => {
-            this.allManagerNodes = data;
-        });
+    async mounted() {
+        await this.fetchManagerNodes();
     },
     methods: {
-        formatDateToGermanLocale(date: Date): String {
+        async fetchManagerNodes(): Promise<void> {
+            this.allManagerNodes = await TestDataService.getNodes();
+        },
+        formatDateToGermanLocale(date: Date): string {
             return MomentWrapper.formatDateToGermanLocale(date);
         },
         updateGlobalFilter(searchTerm: string) {
             this.filters.global.value = searchTerm;
         },
-        handleModelValueChange(newModelValue: ManagerNode[]) {
-            this.$emit("update:modelValue", newModelValue);
+        onTargetNodeIdsChange(newModelValue: ManagerNode[]) {
+            this.$emit("update:targetNodeIds", newModelValue);
+        },
+        getNodeStatusInfoForNode(nodeId: number): NodeStatusInfo {
+            return this.execution.nodeStatusInfos.find(
+                (info) => info.nodeId === nodeId
+            );
+        },
+        showStatusMessage(nodeId: number) {
+            const nodeInfo = this.getNodeStatusInfoForNode(nodeId);
+            if (nodeInfo && nodeInfo.statusMessage) {
+                const newWindow = window.open("", "_blank");
+                newWindow.document.write(
+                    `<pre>${nodeInfo.statusMessage}</pre>`
+                );
+                newWindow.document.title = `Status Message for Node ${nodeId}`;
+                newWindow.document.close();
+            }
         },
     },
 };

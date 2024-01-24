248f9890d5a2dcca657dc09ecb1fa7d991382f4a
Alexander Kombeiz <alexkombie@gmail.com>
2024-01-24 10:58:02 +0100

    merged editTargetNodes with RequestTargetNodes

--- ui/src/components/targetNodes/RequestTargetNodes.vue
+++ ui/src/components/targetNodes/RequestTargetNodes.vue
@@ -1,13 +1,40 @@
 <template>
     <Fieldset :legend="$t('targetNodes')" :toggleable="true">
         <template v-if="editable">
-            <!--
-        <EditTargetNodes
-            :modelValue="targetNodeIds"
-            :allManagerNodes="allManagerNodes"
-            @update:modelValue="onTargetNodeIdsChange"
-        />
-        -->
+            <SearchInput
+                class="flex flex-wrap justify-content-end mb-2"
+                @update:input="filterPickListNodes"
+            />
+            <!-- TODO Optional: red/green color for items in source/target -->
+            <!-- TOOD order by id when moving-->
+            <!-- TODO increase height -->
+            <PickList
+                v-model="pickListNodes"
+                @update:modelValue="handleListChange"
+                dataKey="id"
+                :showSourceControls="false"
+                :showTargetControls="false"
+                :targetListProps="{ class: 'h-30rem' }"
+                :sourceListProps="{ class: 'h-30rem' }"
+                :moveToTargetProps="{ class: 'bg-red-600' }"
+                :moveAllToTargetProps="{ class: 'bg-red-600' }"
+                :moveToSourceProps="{ class: 'bg-green-500' }"
+                :moveAllToSourceProps="{ class: 'bg-green-500' }"
+            >
+                <template #sourceheader>{{ $t("selectedNodes") }}</template>
+                <template #targetheader>{{ $t("availableNodes") }}</template>
+                <template #item="slotProps">
+                    <div class="flex flex-wrap flex-column">
+                        <span class="font-bold">
+                            [{{ slotProps.item.id }}]
+                            {{ slotProps.item.clientDN.CN }}
+                        </span>
+                        <span class="flex flex-wrap">
+                            <TagList :tags="slotProps.item.tags" />
+                        </span>
+                    </div>
+                </template>
+            </PickList>
         </template>
         <template v-else>
             <div class="flex flex-wrap justify-content-between mb-2">
@@ -22,26 +49,20 @@
                         {{ execution.nodeStatusInfos.length }}
                     </p>
                 </div>
-                <SearchInput @update:input="updateGlobalFilter" />
+                <SearchInput @update:input="filterDataTableNodes" />
             </div>
 
             <DataTable
                 ref="dt"
-                v-model:filters="filters"
-                :value="selectedNodes"
+                class="h-34rem"
+                :value="dataTableNodes"
                 sortField="id"
                 :sortOrder="1"
-                :globalFilterFields="[
-                    'id',
-                    'clientDN.L',
-                    'tags',
-                    'lastContact',
-                    'state',
-                ]"
                 scrollable
+                scrollHeight="500px"
             >
                 <Column field="id" :header="$t('id')" sortable />
-                <Column field="clientDN.L" :header="$t('node')" sortable />
+                <Column field="clientDN.CN" :header="$t('node')" sortable />
                 <Column field="tags" :header="$t('tags')" sortable>
                     <template #body="slotProps">
                         <TagList :tags="slotProps.data.tags" />
@@ -83,11 +104,15 @@
                         </template>
                     </Column>
                 </template>
+                <!-- TODO edge case if only 1 node -->
+                <template #footer>
+                    {{
+                        $t("xNodes", {
+                            numNodes: selectedNodes ? selectedNodes.length : 0,
+                        })
+                    }}
+                </template>
             </DataTable>
-            <!-- TODO make globalFilterFields conditionally -->
-            <!-- TODO Search does not work for date -->
-
-            <!-- Filter by Tag does not work -->
             <!-- ToDo export table does not work???-->
             <ExportTableButton class="mt-3" :datatableRef="$refs.dt" />
         </template>
@@ -99,7 +124,7 @@ import Fieldset from "primevue/fieldset";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import Button from "primevue/button";
-import { FilterMatchMode } from "primevue/api";
+import PickList from "primevue/picklist";
 
 import { ManagerNode, RequestExecution, NodeStatusInfo } from "@/utils/Types";
 import { RequestState } from "@/utils/Enums";
@@ -107,7 +132,6 @@ import MomentWrapper from "@/utils/MomentWrapper";
 import { TestDataService } from "@/services/TestDataService";
 import TagList from "@/components/tags/TagList.vue";
 import ExportTableButton from "./ExportTableButton.vue";
-import EditTargetNodes from "./EditTargetNodes.vue";
 import SearchInput from "./SearchInput.vue";
 import NodeStatusInfoTimeline from "./NodeStatusInfoTimeline.vue";
 
@@ -117,8 +141,8 @@ export default {
         DataTable,
         Column,
         Button,
+        PickList,
         TagList,
-        EditTargetNodes,
         ExportTableButton,
         SearchInput,
         NodeStatusInfoTimeline,
@@ -144,16 +168,20 @@ export default {
     data() {
         return {
             allManagerNodes: [] as ManagerNode[],
-            filters: {
-                global: { value: "", matchMode: FilterMatchMode.CONTAINS },
-            },
+            pickListNodes: [[] as ManagerNode[], [] as ManagerNode[]],
+            dataTableNodes: [] as ManagerNode[],
         };
     },
     computed: {
         selectedNodes(): ManagerNode[] {
-            return this.allManagerNodes.filter((node) =>
-                this.targetNodeIds.has(node.id)
-            );
+            return this.allManagerNodes
+                .filter((node) => this.targetNodeIds.has(node.id))
+                .sort((a, b) => a.id - b.id);
+        },
+        availableNodes(): ManagerNode[] {
+            return this.allManagerNodes
+                .filter((node) => !this.targetNodeIds.has(node.id))
+                .sort((a, b) => a.id - b.id);
         },
         requestExecutionOnNodes() {
             return this.execution.nodeStatusInfos.filter(
@@ -163,6 +191,8 @@ export default {
     },
     async mounted() {
         await this.fetchManagerNodes();
+        this.filterPickListNodes("");
+        this.filterDataTableNodes("");
     },
     methods: {
         async fetchManagerNodes(): Promise<void> {
@@ -171,11 +201,57 @@ export default {
         formatDateToGermanLocale(date: Date): string {
             return MomentWrapper.formatDateToGermanLocale(date);
         },
-        updateGlobalFilter(searchTerm: string) {
-            this.filters.global.value = searchTerm;
+        filterDataTableNodes(searchTerm: string) {
+            this.dataTableNodes = this.filterNodesBySearchTerm(
+                this.selectedNodes,
+                searchTerm
+            );
         },
-        onTargetNodeIdsChange(newModelValue: ManagerNode[]) {
-            this.$emit("update:targetNodeIds", newModelValue);
+        filterPickListNodes(searchTerm: string) {
+            const filteredSelectedNodes = this.filterNodesBySearchTerm(
+                this.selectedNodes,
+                searchTerm
+            );
+            const filteredAvailableNodes = this.filterNodesBySearchTerm(
+                this.availableNodes,
+                searchTerm
+            );
+            this.pickListNodes = [
+                filteredSelectedNodes,
+                filteredAvailableNodes,
+            ];
+        },
+        // TODO add filter by german date
+        filterNodesBySearchTerm(
+            nodes: ManagerNode[],
+            searchTerm: string
+        ): ManagerNode[] {
+            if (!searchTerm) return nodes;
+            return nodes.filter(
+                (node) =>
+                    node.id
+                        .toString()
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase()) ||
+                    [...node.tags].some((tag) =>
+                        tag.toLowerCase().includes(searchTerm.toLowerCase())
+                    ) ||
+                    node.clientDN.CN.toLowerCase().includes(
+                        searchTerm.toLowerCase()
+                    )
+            );
+        },
+        handleListChange() {
+            let updatedNodeIds = [...this.targetNodeIds];
+            this.pickListNodes[0].forEach((node) => {
+                if (!updatedNodeIds.includes(node.id)) {
+                    updatedNodeIds.push(node.id);
+                }
+            });
+            updatedNodeIds = updatedNodeIds.filter(
+                (id) => !this.pickListNodes[1].some((node) => node.id === id)
+            );
+            this.$emit("update:targetNodeIds", updatedNodeIds);
         },
         isNotDraft(): boolean {
             return !(this.requestState === RequestState.DRAFT);
@@ -195,7 +271,9 @@ export default {
                     newWindow.document.write(
                         `<pre>${nodeInfo.statusMessage}</pre>`
                     );
-                    newWindow.document.title = `Status Message for Node ${nodeId}`;
+                    newWindow.document.title = this.$t("nodeStatusMessage", {
+                        nodeId: nodeId,
+                    });
                     newWindow.document.close();
                 }
             }

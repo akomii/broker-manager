73c766bfe71ffa70fd4d37411596a2ff20151547
Alexander Kombeiz <alexkombie@gmail.com>
2024-02-06 09:26:42 +0100

    made icons smaller

--- ui/src/components/tables/AnchoredRequestIcon.vue
+++ ui/src/components/tables/AnchoredRequestIcon.vue
@@ -2,6 +2,6 @@
     <i
         style="color: var(--primary-color)"
         v-tooltip.bottom="$t('anchoredRequest')"
-        class="pi pi-star-fill text-xl"
+        class="pi pi-star-fill"
     />
 </template>

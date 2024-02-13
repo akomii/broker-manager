0853b7fff63c972a9600442f73012973daa3db69
Alexander Kombeiz <alexkombie@gmail.com>
2024-02-13 15:49:03 +0100

    renamed tables/ to executions/

--- ui/src/views/requests/demoView.vue
+++ ui/src/views/requests/demoView.vue
@@ -86,7 +86,7 @@ import TargetNodesView from "@/components/targetNodes/TargetNodesView.vue";
 import Button from "primevue/button";
 import ProgressSpinner from "primevue/progressspinner";
 import Divider from "primevue/divider";
-import ExecutionTableView from "@/components/tables/ExecutionTableView.vue";
+import ExecutionTableView from "@/components/executions/ExecutionTableView.vue";
 
 import { TestDataService } from "@/services/TestDataService";
 import { Request, RequestExecution } from "@/utils/Types";

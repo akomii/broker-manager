ec20b1f6fc064dc85a8ca9c699450189b32fbc91
Alexander Kombeiz <alexkombie@gmail.com>
2024-02-14 15:09:07 +0100

    Add SimpleChipList component

---
+++ ui/src/components/tags/SimpleChipList.vue
@@ -0,0 +1,21 @@
+<template>
+    <Chip v-for="chip in chips" :key="chip" class="py-1 px-2 m-1">
+        <span class="text-sm"> {{ chip }} </span>
+    </Chip>
+</template>
+
+<script lang="ts">
+import Chip from "primevue/chip";
+
+export default {
+    components: {
+        Chip,
+    },
+    props: {
+        chips: {
+            type: Array<string>,
+            required: true,
+        },
+    },
+};
+</script>

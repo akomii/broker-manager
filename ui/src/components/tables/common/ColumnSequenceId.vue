221e95ed4c016e62af6f508c62df20beeab0fc0f
Alexander Kombeiz <alexkombie@gmail.com>
2024-02-14 19:29:49 +0100

    Add icons for results download status and new results availability

--- ui/src/components/tables/common/ColumnSequenceId.vue
+++ ui/src/components/tables/common/ColumnSequenceId.vue
@@ -5,19 +5,42 @@
                 <span>
                     {{ slotProps.data.sequenceId }}
                 </span>
+                <ResultsNotDownloadedIcon
+                    v-if="
+                        isResultsDownloadLogEmpty(
+                            slotProps.data.resultsDownloadLog
+                        ) &&
+                        isExecutionNotPending(slotProps.data.executionState)
+                    "
+                />
+                TODO
+                <!-- TODO new Results Available-->
+                <NewResultsAvailableIcon />
             </div>
         </template>
     </Column>
 </template>
 
-<!-- TODO new Results Available-->
-<!-- TODO no results downloaded -->
 <script lang="ts">
 import Column from "primevue/column";
+import ResultsNotDownloadedIcon from "@/components/icons/ResultsNotDownloadedIcon.vue";
+import NewResultsAvailableIcon from "@/components/icons/NewResultsAvailableIcon.vue";
+import { ResultsDownloadLog } from "@/types/RequestExecution";
+import { ExecutionState } from "@/utils/Enums.ts";
 
 export default {
     components: {
         Column,
+        ResultsNotDownloadedIcon,
+        NewResultsAvailableIcon,
+    },
+    methods: {
+        isResultsDownloadLogEmpty(downloadLog: ResultsDownloadLog[]): boolean {
+            return downloadLog.length === 0;
+        },
+        isExecutionNotPending(state: ExecutionState): boolean {
+            return state !== ExecutionState.PENDING;
+        },
     },
 };
 </script>

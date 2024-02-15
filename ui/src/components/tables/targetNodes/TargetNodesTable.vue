93ddc2aa8b2e1ae8a708a068f3b9cf5b0905c5d5
Alexander Kombeiz <alexkombie@gmail.com>
2024-02-15 13:47:45 +0100

    removed unused imports from TargetNodesTable

--- ui/src/components/tables/targetNodes/TargetNodesTable.vue
+++ ui/src/components/tables/targetNodes/TargetNodesTable.vue
@@ -42,48 +42,32 @@
             key="nodeProcessingState"
         />
         <template #empty>
-            <p class="flex justify-content-center">
-                {{ $t("noNodesFound") }}
-            </p>
+            <p class="flex justify-content-center">{{ $t("noNodesFound") }}</p>
         </template>
         <template #footer>
             <TableFooter :refDataTable="$refs.targetNodesTable">
-                <template #count-message>
-                    {{ nodesCountMessage }}
-                </template>
+                <template #count-message>{{ nodesCountMessage }}</template>
             </TableFooter>
         </template>
     </DataTable>
 </template>
 
 <script lang="ts">
-import Fieldset from "primevue/fieldset";
 import DataTable from "primevue/datatable";
-import Column from "primevue/column";
-import Button from "primevue/button";
-import EditableTagListView from "@/components/tags/EditableTagListView.vue";
-import { ManagerNode, NodeStatusInfo } from "@/utils/Types";
-import MomentWrapper from "@/utils/MomentWrapper";
-import ExportTableButton from "@/components/buttons/ExportTableButton.vue";
 import SearchInput from "@/components/tables/SearchInput.vue";
-import NodeStatusInfoTimelineButton from "@/components/buttons/NodeStatusInfoTimelineButton.vue";
 import TableFooter from "@/components/tables/TableFooter.vue";
 import ColumnNodeId from "@/components/tables/nodeColumns/ColumnNodeId.vue";
 import ColumnCommonName from "@/components/tables/nodeColumns/ColumnCommonName.vue";
 import ColumnNodeTags from "@/components/tables/nodeColumns/ColumnNodeTags.vue";
 import ColumnLastContact from "@/components/tables/nodeColumns/ColumnLastContact.vue";
 import ColumnsNodeProcessingState from "@/components/tables/targetNodes/ColumnsNodeProcessingState.vue";
+import MomentWrapper from "@/utils/MomentWrapper";
+import { ManagerNode, NodeStatusInfo } from "@/utils/Types";
 
 export default {
     components: {
-        Fieldset,
         DataTable,
-        Column,
-        Button,
-        EditableTagListView,
-        ExportTableButton,
         SearchInput,
-        NodeStatusInfoTimelineButton,
         TableFooter,
         ColumnNodeId,
         ColumnCommonName,
@@ -109,7 +93,7 @@ export default {
             default: false,
         },
         dataTableHeight: {
-            type: String as () => string,
+            type: String,
             required: true,
         },
     },
@@ -134,13 +118,13 @@ export default {
             return this.enrichedTargetNodes.filter((node) => {
                 const searchFields = [
                     node.id.toString(),
-                    node.clientDN.CN,
-                    node.tags.map((tag) => tag).join(" "),
+                    node.clientDN?.CN,
+                    node.tags.join(" "),
                     MomentWrapper.formatDateToGermanLocale(node.lastContact),
                 ];
                 return searchFields.some((field) =>
                     field
-                        ?.toLowerCase()
+                        .toLowerCase()
                         .includes(this.currentSearchTerm.toLowerCase())
                 );
             });

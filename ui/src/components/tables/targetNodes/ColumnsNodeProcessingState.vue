cfd9dd8958e73fc8942e11ad2da4354eba4a44ea
Alexander Kombeiz <alexkombie@gmail.com>
2024-02-15 10:14:15 +0100

    Created Column for Node Timeline and Status Message

---
+++ ui/src/components/tables/targetNodes/ColumnsNodeProcessingState.vue
@@ -0,0 +1,30 @@
+<template>
+    <Column field="state" :header="$t('processingState')">
+        <template #body="slotProps">
+            <NodeStatusInfoTimelineButton
+                :nodeStatusInfo="slotProps.data.nodeStatusInfo"
+            />
+        </template>
+    </Column>
+    <Column field="msg" header="">
+        <template #body="slotProps">
+            <NodeStatusMessageButton
+                :statusMessage="slotProps.data.nodeStatusInfo.statusMessage"
+            />
+        </template>
+    </Column>
+</template>
+
+<script lang="ts">
+import Column from "primevue/column";
+import NodeStatusInfoTimelineButton from "@/components/buttons/NodeStatusInfoTimelineButton.vue";
+import NodeStatusMessageButton from "@/components/buttons/NodeStatusMessageButton.vue";
+
+export default {
+    components: {
+        Column,
+        NodeStatusInfoTimelineButton,
+        NodeStatusMessageButton,
+    },
+};
+</script>

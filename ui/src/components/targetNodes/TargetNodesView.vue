4d848b1d4a125ee342df6951616f8b5e9c68f42d
Alexander Kombeiz <alexkombie@gmail.com>
2024-02-15 10:59:47 +0100

    integrated TargetNodesTable in TargetNodesView

--- ui/src/components/targetNodes/TargetNodesView.vue
+++ ui/src/components/targetNodes/TargetNodesView.vue
@@ -1,8 +1,8 @@
 <template>
     <Fieldset :legend="$t('targetNodes')" :class="fieldSetHeight">
         <TargetNodesTable
-            :targetNodes="[]"
-            :targetNodeStatusInfos="[]"
+            :targetNodes="targetNodes"
+            :targetNodeStatusInfos="targetNodeStatusInfos"
             :sequenceId="execution.sequenceId"
             :showProcessingStateInfo="showProcessingStateInfo"
             :dataTableHeight="dataTableHeight"
@@ -12,26 +12,13 @@
 
 <script lang="ts">
 import Fieldset from "primevue/fieldset";
-import DataTable from "primevue/datatable";
-import Column from "primevue/column";
-import Button from "primevue/button";
-import EditableTagListView from "@/components/tags/EditableTagListView.vue";
-import { ManagerNode, RequestExecution, NodeStatusInfo } from "@/utils/Types";
-import MomentWrapper from "@/utils/MomentWrapper";
-import ExportTableButton from "@/components/common/ExportTableButton.vue";
-import SearchInput from "@/components/common/SearchInput.vue";
 import TargetNodesTable from "@/components/tables/targetNodes/TargetNodesTable.vue";
+import { ManagerNode, RequestExecution, NodeStatusInfo } from "@/utils/Types";
 import { TestDataService } from "@/services/TestDataService";
 
 export default {
     components: {
         Fieldset,
-        DataTable,
-        Column,
-        Button,
-        EditableTagListView,
-        ExportTableButton,
-        SearchInput,
         TargetNodesTable,
     },
     props: {
@@ -54,90 +41,32 @@ export default {
     },
     data() {
         return {
-            dataTableNodes: [] as ManagerNode[],
             allManagerNodes: [] as ManagerNode[],
         };
     },
     computed: {
-        requestExecutionOnNodes(): number {
-            return this.execution.nodeStatusInfos.filter(
-                (nodeInfo) => nodeInfo.completed !== null
-            ).length;
-        },
         dataTableHeight(): string {
             const heightNumber = parseInt(this.fieldSetHeight.split("-")[1]);
             return `h-${heightNumber - 10}-4rem`;
+            // TODO return `calc(${this.fieldSetHeight} - 10rem)`;
         },
-        selectedNodes(): ManagerNode[] {
-            return this.sortNodesById(
-                this.allManagerNodes.filter((node) =>
-                    this.targetNodeIds.includes(node.id)
-                )
+        targetNodes(): ManagerNode[] {
+            return this.allManagerNodes.filter((node) =>
+                this.targetNodeIds.includes(node.id)
             );
         },
-        availableNodes(): ManagerNode[] {
-            return this.sortNodesById(
-                this.allManagerNodes.filter(
-                    (node) => !this.targetNodeIds.includes(node.id)
-                )
+        targetNodeStatusInfos(): NodeStatusInfo[] {
+            return this.execution.nodeStatusInfos.filter((info) =>
+                this.targetNodeIds.includes(info.nodeId)
             );
         },
     },
     async mounted() {
         await this.fetchManagerNodes();
-        this.filterDataTableNodes("");
     },
     methods: {
         async fetchManagerNodes(): Promise<void> {
             this.allManagerNodes = await TestDataService.getNodes();
-        },
-        sortNodesById(nodes: ManagerNode[]): ManagerNode[] {
-            return nodes.sort((a, b) => a.id - b.id);
-        },
-        // TODO add filter by german date
-        filterNodesBySearchTerm(
-            nodes: ManagerNode[],
-            searchTerm: string
-        ): ManagerNode[] {
-            return nodes.filter(
-                (node) =>
-                    node.id.toString().includes(searchTerm) ||
-                    node.tags.some((tag) =>
-                        tag.toLowerCase().includes(searchTerm)
-                    ) ||
-                    node.clientDN.CN.toLowerCase().includes(searchTerm)
-            );
-        },
-        filterDataTableNodes(searchTerm: string) {
-            this.dataTableNodes = this.filterNodesBySearchTerm(
-                this.selectedNodes,
-                searchTerm.toLowerCase()
-            );
-        },
-        formatDateToGermanLocale(date: Date): string {
-            return MomentWrapper.formatDateToGermanLocale(date);
-        },
-        showStatusMessage(nodeId: number) {
-            const nodeInfo = this.getNodeStatusInfoForNode(nodeId);
-            if (nodeInfo?.statusMessage) {
-                const newWindow = window.open("", "_blank");
-                if (newWindow) {
-                    newWindow.document.write(
-                        `<pre>${nodeInfo.statusMessage}</pre>`
-                    );
-                    newWindow.document.title = this.$t("nodeStatusMessage", {
-                        nodeId: nodeId,
-                    });
-                    newWindow.document.close();
-                }
-            }
-        },
-        getNodeStatusInfoForNode(nodeId: number): NodeStatusInfo {
-            return (
-                this.execution.nodeStatusInfos.find(
-                    (info) => info.nodeId === nodeId
-                ) || ({} as NodeStatusInfo)
-            );
         },
     },
 };

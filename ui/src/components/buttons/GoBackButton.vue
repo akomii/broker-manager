a0e91b9313c20b5119604dd3a8a7348175cb4094
Alexander Kombeiz <alexkombie@gmail.com>
2024-02-19 14:39:28 +0100

    added functionality to GoBackButton

--- ui/src/components/buttons/GoBackButton.vue
+++ ui/src/components/buttons/GoBackButton.vue
@@ -1,12 +1,26 @@
 <template>
-    <Button label="TODO" icon="pi pi-chevron-left text-xl" outlined />
+    <Button
+        icon="pi pi-chevron-left text-xl"
+        outlined
+        v-tooltip.bottom="$t('back')"
+        @click="goBack"
+    />
 </template>
 
 <script lang="ts">
 import Button from "primevue/button";
+import { useRouter } from "vue-router";
+
 export default {
     components: {
         Button,
+    },
+    setup() {
+        const router = useRouter();
+        function goBack() {
+            router.go(-1);
+        }
+        return { goBack };
     },
 };
 </script>

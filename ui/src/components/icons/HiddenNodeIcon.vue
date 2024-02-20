2cf0a99ae60d816e07cd5180895d1a89201d8444
Alexander Kombeiz <alexkombie@gmail.com>
2024-02-20 12:35:54 +0100

    added icon after column name if node has no api key

---
+++ ui/src/components/icons/HiddenNodeIcon.vue
@@ -0,0 +1,6 @@
+<template>
+    <i
+        v-tooltip.right="$t('nodeHasNoApiKey')"
+        class="pi pi-eye-slash text-blue-700"
+    />
+</template>
--- ui/src/components/tableColumns/managerNodeColumns/ColumnCommonName.vue
+++ ui/src/components/tableColumns/managerNodeColumns/ColumnCommonName.vue
@@ -1,13 +1,24 @@
 <template>
-    <Column field="clientDN.CN" :header="$t('node')" sortable />
+    <Column :header="$t('node')" sortable>
+        <template #body="slotProps">
+            <div class="flex gap-1">
+                <span>
+                    {{ slotProps.data.clientDN.CN }}
+                </span>
+                <HiddenNodeIcon v-if="!slotProps.data.apiKey" />
+            </div>
+        </template>
+    </Column>
 </template>
 
 <script lang="ts">
 import Column from "primevue/column";
+import HiddenNodeIcon from "@/components/icons/HiddenNodeIcon.vue";
 
 export default {
     components: {
         Column,
+        HiddenNodeIcon,
     },
 };
 </script>

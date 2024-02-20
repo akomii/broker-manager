242f7bca6176c7e8ff0b8181f9d009fdc85f788d
Alexander Kombeiz <alexkombie@gmail.com>
2024-02-20 11:10:18 +0100

    refactored requests table header

--- ui/src/components/tables/requestsTable/RequestsTableHeader.vue
+++ ui/src/components/tables/requestsTable/RequestsTableHeader.vue
@@ -1,46 +1,52 @@
 <template>
-    <div class="flex flex-wrap justify-content-between align-items-center">
-        <div>
-            <span class="field-checkbox">
-                <Checkbox
-                    v-model="showArchived"
-                    :binary="true"
-                    inputId="toggleArchived"
-                    @update:modelValue="emitToggleArchived"
-                />
-                <label for="toggleArchived" class="ml-2">
-                    {{ $t("showArchivedRequests") }}
-                </label>
-            </span>
-            <span class="field-checkbox">
-                <Checkbox
-                    v-model="showDraft"
-                    :binary="true"
-                    inputId="toggleDraft"
-                    @update:modelValue="emitToggleDraft"
-                />
-                <label for="toggleDraft" class="ml-2">
-                    {{ $t("showDrafts") }}
-                </label>
-            </span>
-        </div>
-        <span class="flex gap-2 align-items-center">
-            <SearchInput @update:input="emitSearch" />
+    <TableHeaderCommon @search="$emit('search', $event)">
+        <template #title>
+            <h2 class="text-700">{{ $t("requestsOverview") }}</h2>
+        </template>
+
+        <template #filter-controls>
+            <div class="grid">
+                <span class="col-12">
+                    <Checkbox
+                        v-model="showArchived"
+                        :binary="true"
+                        inputId="toggleArchived"
+                        @update:modelValue="emitToggleArchived"
+                    />
+                    <label for="toggleArchived" class="ml-2">
+                        {{ $t("showArchivedRequests") }}
+                    </label>
+                </span>
+                <span class="col-12">
+                    <Checkbox
+                        v-model="showDraft"
+                        :binary="true"
+                        inputId="toggleDraft"
+                        @update:modelValue="emitToggleDraft"
+                    />
+                    <label for="toggleDraft" class="ml-2">
+                        {{ $t("showDrafts") }}
+                    </label>
+                </span>
+            </div>
+        </template>
+
+        <template #new-item-actions>
             <NewDraftButton />
-        </span>
-    </div>
+        </template>
+    </TableHeaderCommon>
 </template>
 
 <script lang="ts">
 import Checkbox from "primevue/checkbox";
+import TableHeaderCommon from "@/components/tables/TableHeaderCommon.vue";
 import NewDraftButton from "@/components/buttons/NewDraftButton.vue";
-import SearchInput from "@/components/tables/SearchInput.vue";
 
 export default {
     components: {
         Checkbox,
+        TableHeaderCommon,
         NewDraftButton,
-        SearchInput,
     },
     data() {
         return {
@@ -54,9 +60,6 @@ export default {
         },
         emitToggleDraft(val: boolean) {
             this.$emit("update:showDraft", val);
-        },
-        emitSearch(val: String) {
-            this.$emit("search", val);
         },
     },
 };

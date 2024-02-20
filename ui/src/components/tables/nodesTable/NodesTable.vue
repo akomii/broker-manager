f7ce188f1f2a332e2bbe8bc92184a800a54e1daf
Alexander Kombeiz <alexkombie@gmail.com>
2024-02-20 11:32:00 +0100

    init basic nodes table

---
+++ ui/src/components/tables/nodesTable/NodesTable.vue
@@ -0,0 +1,18 @@
+<template>{{ nodes }}</template>
+
+<script lang="ts">
+import { ManagerNode } from "@/util/Types.ts";
+
+export default {
+    components: {},
+    props: {
+        nodes: {
+            type: Array as () => ManagerNode[],
+            required: true,
+        },
+    },
+    data() {
+        return {};
+    },
+};
+</script>
--- ui/src/views/nodes/nodes.vue
+++ ui/src/views/nodes/nodes.vue
@@ -1,3 +1,28 @@
 <template>
-    <p>ToDo</p>
+    <NodesTable :nodes="allManagerNodes" class="mx-3" />
 </template>
+
+<script lang="ts">
+import { TestDataService } from "@/services/TestDataService";
+import { ManagerNode } from "@/utils/Types";
+import NodesTable from "@/components/tables/nodesTable/NodesTable.vue";
+
+export default {
+    components: {
+        NodesTable,
+    },
+    data() {
+        return {
+            allManagerNodes: [] as ManagerNode[],
+        };
+    },
+    async mounted() {
+        await this.fetchManagerNodes();
+    },
+    methods: {
+        async fetchManagerNodes(): Promise<void> {
+            this.allManagerNodes = await TestDataService.getNodes();
+        },
+    },
+};
+</script>

168913eaf41298c68e123c69a5910f68466e768f
Alexander Kombeiz <alexkombie@gmail.com>
2024-02-20 11:48:28 +0100

    added header to nodes table

---
+++ ui/src/components/buttons/AddNewNodeButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <Button :label="$t('addNewNode')" icon="pi pi-plus" severity="success" />
+</template>
+
+<script lang="ts">
+import Button from "primevue/button";
+
+export default {
+    components: {
+        Button,
+    },
+};
+</script>
--- ui/src/components/tables/nodesTable/NodesTable.vue
+++ ui/src/components/tables/nodesTable/NodesTable.vue
@@ -1,10 +1,49 @@
-<template>{{ nodes }}</template>
+<template>
+    <DataTable
+        :value="filteredNodes"
+        sortField="id"
+        :sortOrder="1"
+        ref="nodesTable"
+        paginator
+        :rows="10"
+        :rowsPerPageOptions="[10, 25, 50]"
+    >
+        <template #header>
+            <NodesTableHeader
+                @update:showHidden="showHidden = $event"
+                @search="filterNodes"
+            />
+        </template>
+
+        <!-- COLUMNS HERE-->
+
+        <template #empty>
+            <p class="flex justify-content-center">
+                {{ $t("noNodesFound") }}
+            </p>
+        </template>
+        <template #paginatorstart>
+            <span>{{ nodesCountMessage }}</span>
+        </template>
+        <template #paginatorend>
+            <ExportTableButton class="mt-3" :dt="$refs.nodesTable" />
+        </template>
+    </DataTable>
+</template>
 
 <script lang="ts">
-import { ManagerNode } from "@/util/Types.ts";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import NodesTableHeader from "@/components/tables/nodesTable/NodesTableHeader.vue";
+import ExportTableButton from "@/components/buttons/ExportTableButton.vue";
 
 export default {
-    components: {},
+    components: {
+        DataTable,
+        Column,
+        NodesTableHeader,
+        ExportTableButton,
+    },
     props: {
         nodes: {
             type: Array as () => ManagerNode[],
@@ -12,7 +51,27 @@ export default {
         },
     },
     data() {
-        return {};
+        return {
+            showHidden: false,
+            currentSearchTerm: "",
+        };
+    },
+    computed: {
+        filteredNodes(): ManagerNode[] {
+            //TODO
+            return this.nodes;
+        },
+        nodesCountMessage(): string {
+            const count = this.nodes.length;
+            return count === 1
+                ? this.$t("oneNode")
+                : this.$t("xNodes", { numNodes: count });
+        },
+    },
+    methods: {
+        filterNodes(searchTerm: string = "") {
+            this.currentSearchTerm = searchTerm;
+        },
     },
 };
 </script>

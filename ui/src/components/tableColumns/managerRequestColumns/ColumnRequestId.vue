6ba033c31077f3f7a182efe305794b5db1d7e6fc
Alexander Kombeiz <alexkombie@gmail.com>
2024-02-21 13:25:55 +0100

    added Column for RequestId

---
+++ ui/src/components/tableColumns/managerRequestColumns/ColumnRequestId.vue
@@ -0,0 +1,13 @@
+<template>
+    <Column field="id" :header="$t('requestId')" sortable />
+</template>
+
+<script lang="ts">
+import Column from "primevue/column";
+
+export default {
+    components: {
+        Column,
+    },
+};
+</script>

744df3c4972936bb4a4d20f31d403a2e421eb34f
Alexander Kombeiz <alexkombie@gmail.com>
2024-05-06 10:31:01 +0200

    updated persistence interface and added custom exceptions

--- api/src/main/java/org/aktin/broker/manager/api/persistence/ManagerNodeHandler.java
+++ api/src/main/java/org/aktin/broker/manager/api/persistence/ManagerNodeHandler.java
@@ -8,17 +8,20 @@
  *
  *    This program is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *    GNU Affero General Public License for more details.
  *
  *    You should have received a copy of the GNU Affero General Public License
- *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *    along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package org.aktin.broker.manager.api.persistence;
 
 import java.util.List;
 import java.util.Optional;
+import org.aktin.broker.manager.api.exceptions.DeletePersistedDataException;
+import org.aktin.broker.manager.api.exceptions.PersistDataException;
+import org.aktin.broker.manager.api.exceptions.ReadPersistedDataException;
 import org.aktin.broker.manager.api.models.ManagerNode;
 
 /**
@@ -33,28 +36,32 @@ public interface ManagerNodeHandler {
    * Saves or updates the provided {@link ManagerNode} entity in persistent storage.
    *
    * @param entity The {@link ManagerNode} object to be saved or updated
+   * @throws PersistDataException If there are general problems during the save or update operation
    */
-  void save(ManagerNode entity);
+  void save(ManagerNode entity) throws PersistDataException;
 
   /**
    * Deletes the {@link ManagerNode} with the specified ID from persistent storage.
    *
    * @param id The ID of the {@link ManagerNode} to delete
+   * @throws DeletePersistedDataException If there's an error while deleting the {@link ManagerNode}
    */
-  void delete(int id);
+  void delete(int id) throws DeletePersistedDataException;
 
   /**
    * Retrieves a {@link ManagerNode} from persistent storage based on its ID.
    *
    * @param id The ID of the {@link ManagerNode} to retrieve
    * @return An Optional containing the {@link ManagerNode} if found, or an empty Optional if not
+   * @throws ReadPersistedDataException If there's an error while reading the {@link ManagerNode} data
    */
-  Optional<ManagerNode> get(int id);
+  Optional<ManagerNode> get(int id) throws ReadPersistedDataException;
 
   /**
    * Retrieves all {@link ManagerNode}s from persistent storage.
    *
-   * @return A List containing the {@link ManagerNode}s, potentially an empty list if none exist
+   * @return A List containing the {@link ManagerNode}s, an empty list if none exist
+   * @throws ReadPersistedDataException If there's an error while reading the {@link ManagerNode} data
    */
-  List<ManagerNode> getAll();
+  List<ManagerNode> getAll() throws ReadPersistedDataException;
 }
--- api/src/main/java/org/aktin/broker/manager/api/persistence/ManagerRequestHandler.java
+++ api/src/main/java/org/aktin/broker/manager/api/persistence/ManagerRequestHandler.java
@@ -8,17 +8,20 @@
  *
  *    This program is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *    GNU Affero General Public License for more details.
  *
  *    You should have received a copy of the GNU Affero General Public License
- *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *    along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package org.aktin.broker.manager.api.persistence;
 
 import java.util.List;
 import java.util.Optional;
+import org.aktin.broker.manager.api.exceptions.DeletePersistedDataException;
+import org.aktin.broker.manager.api.exceptions.PersistDataException;
+import org.aktin.broker.manager.api.exceptions.ReadPersistedDataException;
 import org.aktin.broker.manager.api.models.ManagerRequest;
 import org.aktin.broker.query.xml.QuerySchedule;
 
@@ -34,28 +37,32 @@ public interface ManagerRequestHandler {
    * Saves or updates the provided {@link ManagerRequest} entity in persistent storage.
    *
    * @param entity The {@link ManagerRequest} object to be saved or updated
+   * @throws PersistDataException If there are general problems during the save or update operation
    */
-  void save(ManagerRequest<QuerySchedule> entity);
+  void save(ManagerRequest<QuerySchedule> entity) throws PersistDataException;
 
   /**
    * Deletes the {@link ManagerRequest} with the specified ID from persistent storage.
    *
    * @param id The ID of the {@link ManagerRequest} to delete
+   * @throws DeletePersistedDataException If there's an error while deleting the {@link ManagerRequest}
    */
-  void delete(int id);
+  void delete(int id) throws DeletePersistedDataException;
 
   /**
    * Retrieves a {@link ManagerRequest} from persistent storage based on its ID.
    *
    * @param id The ID of the {@link ManagerRequest} to retrieve
    * @return An Optional containing the {@link ManagerRequest} if found, or an empty Optional if not
+   * @throws ReadPersistedDataException If there's an error while reading the {@link ManagerRequest} data
    */
-  Optional<ManagerRequest<QuerySchedule>> get(int id);
+  Optional<ManagerRequest<QuerySchedule>> get(int id) throws ReadPersistedDataException;
 
   /**
    * Retrieves all {@link ManagerRequest}s from persistent storage.
    *
    * @return A List containing the retrieved {@link ManagerRequest}s, potentially an empty list if none exist.
+   * @throws ReadPersistedDataException If there's an error while reading the {@link ManagerRequest} data
    */
-  List<ManagerRequest<QuerySchedule>> getAll();
+  List<ManagerRequest<QuerySchedule>> getAll() throws ReadPersistedDataException;
 }

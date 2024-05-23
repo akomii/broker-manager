0a88b7b09ef7d16082a845b889cd13fac9d799b3
Alexander Kombeiz <alexkombie@gmail.com>
2024-05-23 12:57:11 +0200

    added xml adapters and xslt for validation of ManagerNode

---
+++ persistence/src/main/java/org/aktin/broker/manager/persistence/filesystem/adapters/UserNoteAdapter.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2024 AKTIN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.aktin.broker.manager.persistence.filesystem.adapters;
+
+import jakarta.xml.bind.annotation.adapters.XmlAdapter;
+import org.aktin.broker.manager.persistence.api.models.UserNote;
+import org.aktin.broker.manager.persistence.filesystem.models.FilesystemUserNote;
+
+public class UserNoteAdapter extends XmlAdapter<FilesystemUserNote, UserNote> {
+
+  @Override
+  public UserNote unmarshal(FilesystemUserNote v) {
+    return v;
+  }
+
+  @Override
+  public FilesystemUserNote marshal(UserNote v) throws IllegalArgumentException {
+    if (v instanceof FilesystemUserNote) {
+      return (FilesystemUserNote) v;
+    }
+    throw new IllegalArgumentException("Invalid UserNote instance");
+  }
+}
